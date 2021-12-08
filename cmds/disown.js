253191da4dfeeb66c0a792270139d4ed24f69d12
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-08 16:04:32 +0000

    Fix: disown

--- cmds/disown.js
+++ cmds/disown.js
@@ -1,29 +1,37 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'disown',
-	aliases: ['disown'],
-	description: 'This **deletes** your dragon. **THIS ACTION CAN NOT AND WILL NOT BE UNDONE.**', 
-	category: 'pet',
+	name: "disown",
+	aliases: ["disown"],
+	description: "This **deletes** your dragon. **THIS ACTION CAN NOT AND WILL NOT BE UNDONE.**",
+	category: "pet",
 	cst: "dragon",
-	async run(client, message, args) {
-		let filter = m => m.author.id === message.author.id;
-		const msg = await message.reply("Are you sure you want to disown your dragon? **This action cannot be undone.** Reply with `y`. Anything which is not `y` will be considered as no.");
-		message.channel.awaitMessages(filter, { max: 1, time: 10000, errors: ['time'] }).then(async(c) => {
-			if (c.first().content.toLowerCase().startsWith("y")) {
-		await client.db.delete('pet' + message.author.id);
-			return message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} has disowned their dragon :sob:`)
-			})
-			} else message.reply("You didn't respond with `y`; I'll take that as a no.")
+	async run(client, message) {
+		const pet = await client.db.get("pet" + message.author.id) || client.config.statics.defaults.dragon;
+		const filter = m => m.author.id === message.author.id;
+		await message.reply("Are you sure you want to disown your dragon? **This action cannot and will not be undone.** Reply with `y`. A response other than `y` will be considered as no.");
+		// await here ensures that this message is sent before the collector starts.
+		message.channel.awaitMessages({ filter: filter, max: 1, time: 10_000, errors: ["time"] }).then(async (c) => {
+			if (c.first().content.toLowerCase() == "y") {
+				client.channels.cache.get(client.config.statics.defaults.channels.dsl).send({ content: `Drgn disowned at ${new Date().toISOString()} by U:${message.author.tag}(${message.author.id})\n${pet}` });
+				await client.db.delete("pet" + message.author.id);
+				let cst = await client.db.get("cst" + message.author.id);
+				cst = cst ? cst.split(";") : [];
+				cst = cst.filter((f) => !["dragon"].includes(f));
+				console.log(cst.join(","));
+				await client.db.set("cst" + message.author.id, cst.join(";"));
+				return message.reply({
+					embeds: [
+						new MessageEmbed()
+							.setColor(message.author.color)
+							.setDescription(`${message.author.tag} has disowned their dragon :cry:`),
+					],
+				});
+			}
+			else {message.reply("You didn't respond with `y`; I'll take that as a no.");}
 		})
-			.catch((e) => {
-				message.reply(`${message.author}, You didn't respond in time!`);
-				console.log(
-					e
-				)
-			})
-	}
-}
+			.catch(() => {
+				message.reply("`No response received. Dragon not deleted!`");
+			});
+	},
+};
