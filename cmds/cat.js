32be945e12e01be727373a6e6d0fdd2097f02396
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-08 00:24:58 +0000

    Update cat.js

--- cmds/cat.js
+++ cmds/cat.js
@@ -1,23 +1,24 @@
-const Discord = require('discord.js');
-const fetch = require('node-fetch');
+const { MessageEmbed, Permissions } = require("discord.js");
+const fetch = require("node-fetch");
 
 module.exports = {
 	name: "cat",
 	aliases: ["cat"],
 	category: "fun",
-	description: 'Get a picture of a random cat',
-	usage: 'cat',
-	async run(client, message, args) {
-		if(!message.guild.me.hasPermission("EMBED_LINKS")) {
-			return message.reply("I need the Embed Links permission for this command to work.")
+	description: "Get a picture of a random cat",
+	usage: "cat",
+	async run(client, message) {
+		if (!message.guild.me.permissions.has(Permissions.FLAGS.EMBED_LINKS)) {
+			return message.reply("I need the Embed Links permission for this command to work.");
 		}
-		let data = await fetch("https://api.thecatapi.com/v1/images/search").then(res => res.json());
+		const [{ url }] = await fetch("https://api.thecatapi.com/v1/images/search").then((res) => res.json());
 		message.reply({
-			embed: new Discord.MessageEmbed()
-			.setTitle("Meow")
-			.setImage(data[0].url)
-			.setColor(message.author.color)
-			.setTimestamp()
-		})
+			embeds: [
+				new MessageEmbed()
+					.setTitle("Meow")
+					.setImage(url)
+					.setColor(message.author.color),
+			],
+		});
 	},
-}
+};
