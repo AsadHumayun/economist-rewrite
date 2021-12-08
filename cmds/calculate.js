06c9233d074ccad108baaacd9b7ee9ae8af1f5ea
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-08 07:52:45 +0000

    Fix: calc

--- cmds/calculate.js
+++ cmds/calculate.js
@@ -1,18 +1,20 @@
-const { MessageEmbed, Message } = require('discord.js');
-const math = require('mathjs');
+const { MessageEmbed } = require("discord.js");
+const math = require("mathjs");
 
 module.exports = {
-    name: 'calculate',
-    aliases: [ 'calculate', 'calc', 'math', 'maths' ],
-    description: "Calculates a calculation and returns the numerical answer",
-    async run(client, message, args) {
-        if (!args.length) return message.reply("You must specify a calculation!");
-        const calc = args.join(' ');
-        let number = math.evaluate(calc);
-        message.reply({
-            embed: new MessageEmbed()
-            .setColor(message.author.color)
-            .setDescription(client.config.noExponents(number))
-        })
-    }
-}
+	name: "calculate",
+	aliases: [ "calculate", "calc", "math", "maths" ],
+	description: "Calculates a calculation and returns the numerical answer",
+	async run(client, message, args) {
+		if (!args.length) return message.reply("You must specify a calculation!");
+		const calc = args.join(" ");
+		const number = math.evaluate(calc);
+		message.reply({
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(client.config.noExponents(number)),
+			],
+		});
+	},
+};
