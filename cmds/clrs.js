3a061513283b241003a29b8882d897316fa0a63d
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-08 07:55:43 +0000

    fix: clrs

--- cmds/clrs.js
+++ cmds/clrs.js
@@ -2,24 +2,23 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
 	name: "clrs",
-	category: 'utl',
+	category: "utl",
 	aliases: [ "viewclrs", "clrs", "colors", "colours", "clr" ],
 	description: "View someone's colour preferences",
 	async run(client, message, args) {
-		if (!args) args = [message.author.id]
+		if (!args) args = [message.author.id];
 		let usr = await client.config.fetchUser(args[0])
+		// eslint-disable-next-line no-empty-function, no-unused-vars
 			.catch((x) => {});
 		if (!usr) usr = message.author;
 		const clrs = await client.db.get("clr" + usr.id) || client.config.defaultHexColor;
 		return message.reply({
-			embed: new MessageEmbed()
-			.setColor(message.author.color)
-			.setTitle(
-				`${usr.tag}'s Colour Preferences`
-			)
-			.setDescription(
-				`Every time you use a command, each colour is cycled through sequentially. The last value is where the bot is currently at in your cycle. \n\n\`\`\`js\n${client.inspect(clrs.split(";"))}\`\`\``
-			)
-		});		
-	}
-}
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setTitle(`${usr.tag}'s Colour Preferences`)
+					.setDescription(`Every time you use a command, each colour is cycled through sequentially. The last value is where the bot is currently at in your cycle. \n\n\`\`\`js\n${client.config.Inspect(clrs.split(";"))}\`\`\``),
+			],
+		});
+	},
+};
