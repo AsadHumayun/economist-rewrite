29106b116f375984a72ee51cc326371b21758202
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-08 13:30:24 +0000

    Fix daily

--- cmds/daily.js
+++ cmds/daily.js
@@ -1,27 +1,30 @@
-const { MessageEmbed } = require('discord.js');
-const ms = require('ms');
+const { MessageEmbed } = require("discord.js");
+const ms = require("ms");
 
 module.exports = {
-	name: 'daily',
-	aliases: ['daily'],
-	category: 'ecn',	
+	name: "daily",
+	aliases: ["daily"],
+	category: "ecn",
 	description: "Adds :dollar: 5,000 to your account",
-	async run(client, message, args) {
-		let data = await client.db.get('dlc' + message.author.id);
+	async run(client, message) {
+		let data = await client.db.get("dlc" + message.author.id);
 		data = Number(data);
-		let cd = client.cooldown(message.createdTimestamp, data*client.config.exp);
+		const cd = client.config.cooldown(message.createdTimestamp, data * 60_000);
 		if (cd) {
-				message.reply(`You must wait ${cd} before collecting your daily reward!`);
-		} else {
-				let bal = await client.db.get('bal' + message.author.id) || 0;
-				bal = Number(bal);
-				await client.db.set('dlc' + message.author.id, client.parseCd(message.createdTimestamp, ms("1d")));
-				await client.db.set('bal' + message.author.id, bal + 5000);
-				message.reply({
-					embed: new MessageEmbed()
-					.setDescription(`${message.author.tag} has collected their daily reward and received :dollar: 5,000 in cash`)
-					.setColor(message.author.color)
-				});
-		};
+			message.reply(`You must wait ${cd} before collecting your daily reward!`);
+		}
+		else {
+			let bal = await client.db.get("bal" + message.author.id) || 0;
+			bal = Number(bal);
+			await client.db.set("dlc" + message.author.id, client.config.parseCd(message.createdTimestamp, ms("1d")));
+			await client.db.set("bal" + message.author.id, bal + client.config.statics.dailyReward);
+			message.reply({
+				embeds: [
+					new MessageEmbed()
+						.setColor(message.author.color)
+						.setDescription(`${message.author.tag} has collected their daily reward and received :dollar: ${client.config.comma(client.config.statics.dailyReward)} in cash`),
+				],
+			});
+		}
 	},
 };
