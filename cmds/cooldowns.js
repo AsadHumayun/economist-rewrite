2f5b60c99dca3cef3d5e455a7b3433db97b1c359
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-08 07:51:40 +0000

    Fix: cooldowns.js

--- cmds/cooldowns.js
+++ cmds/cooldowns.js
@@ -1,30 +1,29 @@
-const { MessageEmbed } = require('discord.js');
-const ms = require('ms')
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'cooldowns',
-	aliases: ['cds', 'cooldowns', 'cd', 'coold'],
+	name: "cooldowns",
+	aliases: ["cds", "cooldowns", "cd", "coold"],
 	cst: "supreme",
-	category: 'utl',	
-	async run(client, message, args) {
-		let cds = [];
-		for (cd of client.config.cds) {
+	cstMessage: "You're not prestigious enough to use this command! (requires Supreme)",
+	category: "utl",
+	async run(client, message) {
+		const cds = [];
+		for (let cd of client.config.statics.defaults.cds) {
 			cd = cd.split(";");
-			let cdd = (await client.db.get(cd[0] + message.author.id) || 0);
-			let cdm = client.cooldown(message.createdTimestamp, cdd*client.config.exp, true);
+			const cdd = (await client.db.get(cd[0] + message.author.id) || 0);
+			const cdm = client.config.cooldown(message.createdTimestamp, cdd * 60_000, true);
 			if (cdm) {
-				cds.push(`${cd[1]}: ${cdm}`)
-			};
-		};
-		let chc = await client.db.get("chillc" + message.author.id) || 1;
-				chc = chc * client.config.exp;
-		if (client.cooldown(message.createdTimestamp, chc, true)) cds.push("dose chillpill: " + client.cooldown(message.createdTimestamp, chc, true));
-		if (cds.length < 1) return message.reply(`You have no active cooldowns; displayed cooldowns are [dose chillpill, ${client.config.cds.map((f) => f.split(";")[1]).join(", ")}]`);
+				cds.push(`${cd[1]}: ${cdm}`);
+			}
+		}
+		if (cds.length < 1) return message.reply(`You have no active cooldowns; displayed cooldowns are [dose chillpill, ${client.config.statics.defaults.cds.map((f) => f.split(";")[1]).join(", ")}]`);
 		message.reply({
-			embed: new MessageEmbed()
-			.setColor(message.author.color)
-			.setTitle(`${message.author.tag}'s Cooldowns (${cds.length})`)
-			.setDescription(cds.join("\n"))
-		})
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setTitle(`${message.author.tag}'s Cooldowns (${cds.length})`)
+					.setDescription(cds.join("\n")),
+			],
+		});
 	},
 };
