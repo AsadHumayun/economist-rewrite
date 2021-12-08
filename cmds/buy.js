568b57221cbf668abbd1faa0b3fa4a7130273f94
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-08 13:35:19 +0000

    Update buy.js

--- cmds/buy.js
+++ cmds/buy.js
@@ -1,122 +1,63 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'buy',
-	aliases: ['buy', 'purchase'],
+	name: "buy",
+	aliases: ["buy", "purchase"],
 	description: "Buy something from the overpriced shop",
-	category: 'ecn',
-	usage: '<item>',
-	dev: false,
-	guild: false,
-	disabled: false,
+	category: "ecn",
+	usage: "<item index: number>",
 	async run(client, message, args) {
-		if (isNaN(args[0])) return message.reply("You must provide a valid ID of what you wish to purchase!");
-		let t = parseInt(args[0]);
-		let T = Number(args[0]) - 1;
-		if (T == 100) T = 3;
-		let things = [
-			{ number: 1, price: 25 },
-			{ number: 2, price: 750 },
-			{ number: 3, price: 250 },
-			{ number: 101, price: 100 },
-			{ number: 201, price: 10000 },
-			{ number: 202, price: 2500 },
-			{ number: 4, price: 50_000 }
-		];
-		let valid = [1, 2, 3, 101, 201, 202, 4];
-			if (!t || (!valid.includes(t))) return message.reply("You must provide a valid ID of what you wish to purchase!");
-		let bal = await client.db.get('bal' + message.author.id) || 0;
-		if (!bal || (bal == 0)) return message.reply("You don't have enough :dollar: to purchase that item!");
-		if (bal - things[valid.indexOf(t)].price < 0) return message.reply("You don't have enough :dollar: to purchase that item!");
-		let cst = await client.db.get("cst" + message.author.id) || "";
-				cst = cst.split(";");
-		if (t == 1) {
-			//fishing rod
-			let owns = cst.includes("fishrod")
-			if (!owns) {} else { return message.reply(`${client.config.statics.defaults.emoji.err} You already own a ${client.config.emoji.fishing_rod}`) }
-			await client.db.set('bal' + message.author.id, parseInt(bal - things[valid.indexOf(t)].price));
-			cst.push("fishrod")
-			await client.db.set("cst" + message.author.id, cst.join(";"));
-			message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} has purchased a ${client.config.emoji.fishing_rod} !`)
-			})
-		} else if (t == 2) {
-			//buy phone
-			let owns = cst.includes("phone");
-			if (owns) return message.reply(`${client.config.statics.defaults.emoji.err} You already own a ${client.config.emoji.mobile_phone} !`);
-			await client.db.set('bal' + message.author.id, parseInt(bal - things[valid.indexOf(t)].price))
-			cst.push("phone")
-			let phoneNumber = Math.floor(Math.random() * 100000);
-			phoneNumber = Number(phoneNumber)
-			await client.db.set('n' + phoneNumber, message.author.id);
-			await client.db.set('number' + message.author.id, phoneNumber)
-			await client.db.set("cst" + message.author.id, cst.join(";"));
-			message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} has purchased a ${client.config.emoji.mobile_phone} ! \`${message.guild.prefix}dial\``)
-			})
-		} else if (t == 3) {
-			let owns = cst.includes("phoneb")
-			if (owns) return message.reply(`${client.config.statics.defaults.emoji.err} You already own a ${client.config.emoji.phonebook} !`);
-			cst.push("phoneb")
-			await client.db.set('bal' + message.author.id, parseInt(bal - things[valid.indexOf(t)].price))
-			await client.db.set("cst" + message.author.id, cst.join(";"));
-			message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} has purchased a ${client.config.emoji.phonebook} !`)
-			})
-		} else if (t == 101) {
-			let amt = Number(args[1]);
-			if (!amt) amt = 1;
-			if (isNaN(amt)) return message.reply("The quantity of how many pills you want to buy must be a number");
-			let x = await client.db.get(`chillpills${message.author.id}`) || 0;
-			x = Number(x);
-			if (bal - (things[valid.indexOf(t)].price * amt) < 0) return message.reply(`You don't have enough money to purchase ${amt} chill pills!`);
-			await client.db.set("bal" + message.author.id, bal-things[valid.indexOf(t)].price*amt)
-			await client.db.set(`chillpills${message.author.id}`, parseInt(x + amt));
-			message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} has purchased ${client.config.emoji.chill} ${message.author.com == 1 ? amt : client.comma(amt)}!`)
-			})			
-		} else if (t == 201) {
-			let owns = cst.includes("bvault")
-			if (owns) return message.reply(`${client.config.statics.defaults.emoji.err} You already own a :bank: Bank Vault!`);
-			cst.push("bvault")
-			await client.db.set('bal' + message.author.id, parseInt(bal - things[valid.indexOf(t)].price))
-			await client.db.set("cst" + message.author.id, cst.join(";"));
-			message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} has purchased a :bank: Bank Vault!`)
-			})
-		} else if (t == 202) {
-			let owns = message.author.color == "RANDOM";
-			if (owns) return message.reply(`${client.config.statics.defaults.emoji.err} You already have a random colour preference!`);
-			
-			await client.db.set('bal' + message.author.id, Number(bal - things[valid.indexOf(t)].price))
-			await client.db.set("clr" + message.author.id, "RANDOM;0");
-			message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} has successfully bought a :rainbow: Random Colour Preference!`)
-			})
-		} else if (t == 4) {
-			let owns = cst.includes("slrprmt");
-			if (owns) return message.reply(`${client.config.statics.defaults.emoji.err} You already have a Seller's Permit!`);
-			cst.push("slrprmt");
-			await client.db.set('bal' + message.author.id, parseInt(bal - things[valid.indexOf(t)].price))
-			await client.db.set("cst" + message.author.id, cst.join(";"));
-			
-			message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} has successfully bought a :receipt: Seller's Permit!`)
-			})
+		function alreadyOwned() {
+			return message.reply("You already seem to own that item!");
 		}
+
+		if (!args.length) return message.reply("You must provide a valid ID of what you would like to purchase (the ID of an item is the number in brackets next to that item) in order for this command to work!");
+		const id = Number(args[0]);
+		const items = Object.entries(client.config.statics.shop);
+		// eslint-disable-next-line no-shadow
+		const ids = items.map(([, { id }]) => id);
+		// <LogicError <IfStatement>>: if statement not running
+		// <NOW FIXED>: `id` was not of data type `Number`.
+		if (!ids.includes(id)) return message.channel.send({ content: `Invalid ID "${args[0]}"`, allowedMentions: { parse: [] } });
+		let bal = await client.db.get("bal" + message.author.id) || 0;
+		bal = isNaN(bal) ? 0 : Number(bal);
+		const item = items.find((f) => f[1].id == id);
+		let cst = await client.db.get("cst" + message.author.id);
+		cst = cst ? cst.split(";") : [];
+		if (item[1].method == "cst" && (cst.includes(item[0]))) return alreadyOwned();
+		if (eval(item[1].condt)) return alreadyOwned();
+		if (bal - item[1].price < 0) return message.reply(`You do not have enough money to purchase "${item[1].displayName}"; you need an additional :dollar: ${item[1].price - bal} on top of your current balance in order to purchase this item!`);
+		bal -= item[1].price;
+		if (item[1].method == "cst") {
+			cst.push(item[0]);
+		}
+		else if (item[1].method.split(".").length == 2) {
+			const key = item[1].method.split(".")[0];
+			const indx = item[1].method.split(".")[1];
+
+			let values = await client.db.get(key + message.author.id);
+			values = values ? values.split(";") : Array(indx + 1).fill(0);
+			// prevent an out of bounds type error from occurring by extending the array in a manner such that
+			// arr[indx] is 0 and can thus be incremented without error. (arr[anything in between 0 and indx] is also therefore registered as "")
+			if (!values[indx]) values[indx] = "0";
+			values = values.map((f) => isNaN(f) ? 0 : Number(f));
+			// give the user the ability to specify a number of item that they wish to purchase.
+			// prevents spam, makes the bot look and feel more complete.
+			values[indx] += isNaN(args[1]) ? 1 : Number(args[1]);
+
+			await client.db.set(key + message.author.id, values.join(";"));
+		}
+		else {
+			await item[1].execute(message.author.id);
+		}
+		message.reply({
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(`${message.author.tag} has successfully purchased a ${item[1].emoji} ${item[1].displayName}!`),
+			],
+		});
+		await client.db.set("bal" + message.author.id, bal);
+		await client.db.set("cst" + message.author.id, cst.join(";"));
 	},
 };
