50c6d053e1fd6d651f93e6ff2b358ff941d9d78e
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-08 16:04:38 +0000

    Fix: ditems

--- cmds/ditems.js
+++ cmds/ditems.js
@@ -1,28 +1,30 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'ditems',
-	aliases: ['dperms', 'ditems'],
-	description: "Shows your currently active donor items / bought permissions",
-	category: 'utl',
+	name: "ditems",
+	aliases: ["dperms", "ditems"],
+	description: "Shows your currently active special/donor permissions",
+	category: "utl",
 	async run(client, message, args) {
 		if (!args.length) args = [message.author.id];
-		const user = await client.config.fetchUser(args[0]).catch((x) => {});
+		let user = await client.config.fetchUser(args[0]).catch(() => {return;});
 		if (!user) user = message.author;
 		let cst = await client.db.get("cst" + user.id) || "";
-				cst = cst.split(";");
+		cst = cst.split(";");
 
 		message.reply({
-			embed: new MessageEmbed()
-			.setColor(message.author.color)
-			.setTitle(`${user.tag}'s Donor Ranks`)
-			.setDescription(
-				`
-				\`${message.guild.prefix}ptransfer <user> <item>\` to transfer an item to another account
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setTitle(`${user.tag}'s Donor Ranks`)
+					.setDescription(
+						`
+					\`${message.guild.prefix}ptransfer <user> <item>\` to transfer an item to another account
 
-				\`\`\`\n${client.inspect(cst.filter((x) => client.config.ditems.map((i) => i.split(";")[1]).includes(x)))}\n\`\`\`
-				`
-			)
+					\`\`\`\n${client.config.Inspect(cst.filter((x) => client.config.statics.ditems.map((i) => i.split(";")[1]).includes(x)))}\n\`\`\`
+					`,
+					),
+			],
 		});
 	},
 };
