35c0d30021e2eab1fd78dbbed510f7831e76a160
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-11 22:14:16 +0000

    Update balance.js

--- cmds/balance.js
+++ cmds/balance.js
@@ -1,19 +1,21 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'balance',
-	category: 'ecn',	
-	aliases: ['balance', 'bal', 'money'],
+	name: "balance",
+	category: "ecn",
+	aliases: ["balance", "bal", "money"],
 	description: "Check someone's balance, see how much money they have",
-	usage: '<User(id | @Mention)>',
+	usage: "<User(id | @Mention)>",
 	async run(client, message, args) {
-		let usr = await client.config.fetchUser(args[0]).catch((x) => {});
+		let usr = await client.config.fetchUser(args[0]).catch(() => {return;});
 		if (!usr) usr = message.author;
-		const bal = await client.db.get('bal' + usr.id) || "0";
+		const bal = await client.db.get("bal" + usr.id) || "0";
 		message.reply({
-			embeds: [new MessageEmbed()
-			.setColor(message.author.color)
-			.setDescription(`${usr.tag}'s account contains :dollar: ${client.config.digits(bal)}`)
-			]});
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(`${usr.tag}'s account contains :dollar: ${client.config.digits(bal)}`),
+			],
+		});
 	},
 };
