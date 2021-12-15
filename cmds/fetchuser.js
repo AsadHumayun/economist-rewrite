f874f88f5a0db99383a9108f411c65f2f2421564
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-15 21:37:10 +0000

    Fix: fetchuser

--- cmds/fetchuser.js
+++ cmds/fetchuser.js
@@ -1,40 +1,28 @@
-const { MessageEmbed } = require('discord.js');
-const { inspect } = require('util');
+const { MessageEmbed } = require("discord.js");
+const { inspect } = require("util");
 
 module.exports = {
-	name: 'fetchuser',
-	aliases: ['fetchuser', 'fetch-user', "fu"],
+	name: "fetchuser",
+	aliases: ["fetchuser", "fetch-user", "fu"],
 	category: "own",
 	cst: "administrator132465798",
-	description: 'fetches a user (as partial)',
-	usage: 'fetchuser <id>',
+	description: "fetches a user (as partial)",
+	usage: "fetchuser <id>",
 	async run(client, message, args) {
-		function getUserFromMention(mention) {
-			if (!mention) return;
-			if (mention.startsWith('<@') && mention.endsWith('>')) {
-				mention = mention.slice(2, -1);
-				if (mention.startsWith('!')) {
-					mention = mention.slice(1);
-				}
-				return mention;
-			};
-		};		
-		if (!args.length) return message.reply(`${client.config.statics.defaults.emoji.err} You need mention a user or provide a valid ID!`);
-		const msg = await message.reply(`Fetching user...`);
-		let user;
-			if (message.mentions.users.first()) {
-				user = await client.users.fetch(message.mentions.users.first().id);
-			} else {
-				user = await client.users.fetch(args[0]);
-			};
+		if (!args.length) return message.reply(`${client.config.statics.defaults.emoji.err} You must mention a valid user in order for this command to work!`);
+		const msg = await message.reply("Fetching user...");
+		const user = await client.config.fetchUser(args[0]).catch(() => {return;});
 		if (!user) return msg.edit(`${client.config.statics.defaults.emoji.err} I can't find that user.`);
-		const data = inspect(user, { depth: 100000000 });
-		msg.edit('', { 
-			embed: new MessageEmbed()
-			.setAuthor(user.tag, user.displayAvatarURL())
-			.setColor(message.author.color)
-			.setDescription("```js\n" + data + "\n```")
-			.setFooter(`in ${Date.now() - msg.createdTimestamp} MS`)
+		const data = inspect(user, { depth: 10 });
+		msg.edit({
+			content: null,
+			embeds: [
+				new MessageEmbed()
+					.setAuthor(user.tag, user.displayAvatarURL())
+					.setColor(message.author.color)
+					.setDescription("```js\n" + data + "\n```")
+					.setFooter(`in ${Date.now() - msg.createdTimestamp} MS`),
+			],
 		});
 	},
 };
