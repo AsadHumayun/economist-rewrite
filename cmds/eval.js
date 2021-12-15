4388a05f18bea2e11cb295c58e2aac0d93a2b1b2
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-15 21:35:06 +0000

    Fix: eval

--- cmds/eval.js
+++ cmds/eval.js
@@ -1,66 +1,70 @@
-let Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
 	name: "eval",
-	aliases: ["run", "evaluate", 'eval', 'evalu8'],
+	aliases: ["run", "evaluate", "eval", "evalu8"],
 	desc: "Takes some javascript code and evaluates it! This is limited to our bot developers as it is very powerful.",
 	usage: "eval <code>",
-	category: 'own',
+	category: "own",
 	cst: "administrator132465798",
-async run(client, message, args) {
-	let devs = [client.config.owner, "523579776749928449", "208948873433972737"]
+	async run(client, message, args) {
+		// eval command is very powerful and dangerous.
+		// allows for a user to execute a piece of code.
+		// alllowed people are only myself (the ID in config.js ClientConfiguration class),
+		const devs = [client.config.owner];
 
-	if (!devs.includes(message.author.id)) { // (eval role id) 
-		return message.reply("Wha? Why would you ever want to use this command?")	
-	};
-
-	msg = await message.reply(
-		`**__E__valuating** Please Wait`
-	);
-	async function clean(text) {
-		if (typeof(text) === "string")
-			return await text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-		else
-				return await text;
-	}
-
-	try {
-		async function escapeRegExp(str) {
-			return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1');
+		if (!devs.includes(message.author.id)) {
+			// they get denied access.
+			return message.reply("Wha? Why would you ever want to use this command?");
 		}
-		let code = args.join(" ")
-		if (!code) {
-			return msg.edit("You need to give me some code to evaluate!")
+
+		if (message.emit) {
+			return message.reply("This command cannot be executed on a fakely-emitted `messageCreate` event.");
+		}
+
+		const msg = await message.reply("**__E__valuating** Please Wait");
+		// source: https://github.com/AnIdiotsGuide/discordjs-bot-guide/blob/master/examples/making-an-eval-command.md
+		const clean = async (text) => {
+			if (text && text.constructor.name == "Promise") {text = await text;}
+			if (typeof text !== "string") {text = require("util").inspect(text, { depth: 1 });}
+			text = text
+				.replace(/`/g, "`" + String.fromCharCode(8203))
+				.replace(/@/g, "@" + String.fromCharCode(8203));
+			return text;
 		};
-	//	code = code.toString().replace(new RegExp(escapeRegExp(client.token), 'g'), '/*token removed*/');
-		
-	//	if(code.includes('forEach') && (message.author.id != client.owner)) return msg.edit(process.env.re + ' forEach is forbidden!')
-		if(code.includes('token')) return msg.edit('client token cannot be evaled!')
-		if(code.includes('client[')) return msg.edit('client[ cannot be evaled!')
-		if(code.includes('client [')) return msg.edit('client [ cannot be evaled!')
-		let evaled;
-			if (code instanceof Promise) {
-				evaled = await eval(code);
-			} else {
-				evaled = eval(code);
-			};
-		if (typeof evaled !== "string")
-			evaled = require("util")
-				.inspect(evaled);
-			let cleaned = await clean(evaled);
-		 msg.edit("", {
-			embed: new Discord.MessageEmbed()
-			.setTitle("Evaluation Successful!")
-			.setDescription(`\`\`\`xl\n${cleaned}\n\`\`\``)
-			.setColor(message.author.color)
-		});
-	} catch (err) {
-			msg.edit("", {
-				embed: new Discord.MessageEmbed()
-				.setTitle("Evaluation Unsuccessful")
-				.setDescription(`\`\`\`xl\n${err}\n\`\`\``)
-				.setColor([255, 0, 0])
+		try {
+			const code = args.join(" ");
+			if (!code) {
+				return msg.edit("You need must provide some code to evaluate in order for this command to work!");
+			}
+			const s = Date.now();
+			if (code.includes("token")) return msg.edit("client token cannot be evaled!");
+			if (code.includes("client[")) return msg.edit("client[ cannot be evaled!");
+			if (code.includes("client [")) return msg.edit("client [ cannot be evaled!");
+			const ev = eval(args.join(" "));
+			const cleaned = await clean(ev);
+			msg.edit({
+				content: null,
+				embeds: [
+					new MessageEmbed()
+						.setColor(client.config.statics.defaults.colors.green)
+						.setTitle("Successfully evaluated in " + (Date.now() - s) + " ms")
+						.setFooter(new Date(message.createdTimestamp).toISOString())
+						.setDescription(`\`\`\`\n${client.config.trim(cleaned, 4090)}\n\`\`\``),
+				],
 			});
-		};
+		}
+		catch (err) {
+			msg.edit({
+				content: null,
+				embeds: [
+					new MessageEmbed()
+						.setTitle("Error")
+						.setDescription(`\`\`\`\n${err}\n\`\`\``)
+						.setColor([255, 0, 0])
+						.setFooter(new Date(message.createdTimestamp).toISOString()),
+				],
+			});
+		}
 	},
 };
