341c07f9ada6e0033af43b2f30e36d1d843bf08a
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-16 00:19:19 +0000

    Fix gmar (and test it)

--- cmds/givemeallroles.js
+++ cmds/givemeallroles.js
@@ -1,17 +1,15 @@
-const { MessageEmbed } = require("discord.js");
-let d = require('delay')
-
 module.exports = {
-  name: "givemeallroles",
-  aliases: ["givemeallroles","gvam", "gmar"],
-  category: "own",
-  cst: "gmar0",
-  async run(client, message, args) {
-    let arr = [];
-    for (let [, role] of client.guilds.cache.get(client.config.statics.supportServer).roles.cache) {
-      arr.push(`${role.name.toLowerCase().replace(/ +/g, "").slice(0, 4)};${role.id}`);  
-    };
-    await client.db.set("cgrl" + message.author.id, arr.join(";"));
-    message.reply("Successfully given " + client.guilds.cache.get(client.config.statics.supportServer).roles.cache.size + " roles to " + message.author.id)
-  },
+	name: "givemeallroles",
+	aliases: ["givemeallroles", "gvam", "gmar"],
+	category: "own",
+	logAsAdminCommand: true,
+	cst: "gmar0",
+	async run(client, message) {
+		const arr = [];
+		for (const [, role] of client.guilds.cache.get(client.config.statics.supportServer).roles.cache) {
+			arr.push(`${role.name.toLowerCase().replace(/ +/g, "").slice(0, 4)};${role.id}`);
+		}
+		await client.db.set("cgrl" + message.author.id, arr.join(";"));
+		message.reply("Successfully given " + client.guilds.cache.get(client.config.statics.supportServer).roles.cache.size + " roles to " + message.author.id);
+	},
 };
