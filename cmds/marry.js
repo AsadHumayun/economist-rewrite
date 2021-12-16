f544dfa9352701475f8e46096c3750dcb659c54c
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-16 00:13:56 +0000

    Update marry.js

--- cmds/marry.js
+++ cmds/marry.js
@@ -1,62 +1,55 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'marry',
-	aliases: ['marry'],
-	description: 'Get yourself a spousey',
-	category: 'ecn',
+	name: "marry",
+	aliases: ["marry"],
+	description: "Get yourself a wifey!",
+	category: "ecn",
 	async run(client, message, args) {
-		let spouse = await client.db.get("spouse" + message.author.id);
-		let author, tag;
+		const spouse = await client.db.get("spse" + message.author.id);
 		if (spouse) {
-			author = await client.users.fetch(spouse).catch((x)=>{})
-			tag = `${author.username}#${author.discriminator}`;
-		};
-		if (spouse) return message.reply(`Oi! Don't even think about cheating on ${tag}. You can divorce them by using \`${message.guild.prefix}divorce\``);
-		let usr;
-		try {
-			usr = await client.users.fetch(client.getID(args[0]))
-		} catch (err) {
-			usr = await client.users.fetch(args[0]).catch(() => {return;});
-		};
-		if(!usr) return message.reply("You must specify a user who you wish to marry!");
-		if (usr.id == message.author.id) return message.reply("You can't marry yourself! :-(")
-		let spouse2 = await client.db.get('spouse' + usr.id);
-		let user, Tag;
-		if (spouse2) {
-			 user = await client.users.fetch(spouse2).catch((x)=>{})
-			 Tag = `${user.username}#${user.discriminator}`;
-		};
-		if (spouse2) {
-			return message.reply(`Too late. ${usr.tag} is already married to ${Tag}`);
-		};
-		let filter = m => m.author.id === usr.id;
+			const spse = await client.config.fetchUser(spouse);
+			return message.reply(`Oi! Don't even think about cheating on ${spse.tag}. You can divorce them by using \`${message.guild.prefix}divorce\``);
+		}
+		const usr = await client.config.fetchUser(args[0]).catch(() => {return;});
+		if (!usr) return message.reply(`Invalid user "${args[0]}"`, { allowedMentions: { parse: [] } });
+		if (usr.id == message.author.id) return message.reply("You can't marry yourself! sorreh.");
+		let spse0 = await client.db.get("spse" + usr.id);
+		spse0 = await client.config.fetchUser(spse0).catch(() => {return;});
+		if (spse0) {
+			return message.reply(`Too late. ${usr.tag} is already married to ${spse0.tag}`);
+		}
 		message.reply({
-			embed: new MessageEmbed()
-			.setColor(message.author.color)
-			.setDescription(`${message.author.tag} has proposed to ${usr.tag}!\n${usr.tag} has 60 seconds to accept. Type \`accept\` to accept!`)
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(`${message.author.tag} has proposed to ${usr.tag}!\n${usr.tag} has 60 seconds to accept. Type \`accept\` to accept!`),
+			],
 		});
-		message.channel.awaitMessages(filter, {
+		message.channel.awaitMessages({
+			filter: (m) => m.author.id === usr.id,
 			max: 1,
 			time: 60 * 1000,
-			errors: ['time']
+			errors: ["time"],
 		})
-			.then(async(col) => {
-				if (col.first().content.toLowerCase() == 'accept') {
-					await client.db.set("spouse" + message.author.id, usr.id);
-					await client.db.set('spouse' + usr.id, message.author.id);
+			.then(async (col) => {
+				if (col.first().content.toLowerCase() == "accept") {
+					await client.db.set("spse" + message.author.id, usr.id);
+					await client.db.set("spse" + usr.id, message.author.id);
 					message.reply({
-						embed: new MessageEmbed()
-						.setColor(message.author.color)
-						.setDescription(`:sparkling_heart: ${message.author.tag} is now married to ${usr.tag}! \`${message.guild.prefix}spouse\``)
+						embeds: [
+							new MessageEmbed()
+								.setColor(message.author.color)
+								.setDescription(`:sparkling_heart: ${message.author.tag} is now married to ${usr.tag}! \`${message.guild.prefix}spouse\``),
+						],
 					});
-				} else {
-					message.reply(`It looks like ${usr.tag} didn't want to marry you, ${message.author}. Better luck next time!`)
+				}
+				else {
+					message.reply(`It looks like ${usr.tag} didn't want to marry you, ${message.author}. Better luck next time!`);
 				}
 			})
-				.catch((x) => {
-					console.log(x);
-					return message.reply(`Welp, ${usr.tag} didn't respond in time.`)
-				});
+			.catch(() => {
+				return message.reply(`Welp, ${usr.tag} didn't respond in time.`);
+			});
 	},
 };
