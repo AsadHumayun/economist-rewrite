f131f8ffd35626a1b3329fc653e6555d965d2171
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-16 00:13:34 +0000

    Fix: info

--- cmds/invite.js
+++ cmds/invite.js
@@ -1,15 +1,9 @@
-const { MessageEmbed } = require('discord.js');
-
 module.exports = {
-	name: 'invite',
-	aliases: ['invite', 'inv'],
+	name: "invite",
+	aliases: ["invite", "inv"],
 	description: "View the bot's invite link to add it to other servers",
-	category: 'utl',
-	async run(client, message, ...args) {
-		message.reply({
-			embed: new MessageEmbed()
-			.setColor(message['author']['color'])
-			.setDescription(`[Click here to invite ${client.user.username} to one of your servers!](${client.config.inv})`)
-		})
-	} 
-}
+	category: "utl",
+	async run(client, message) {
+		message.channel.send(`You can invite me to your server by using the following link: <${client.config.inv}>`);
+	},
+};
