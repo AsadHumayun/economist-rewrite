92203f79c33c9a4820d9fc97299e9a0f3f797f0e
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-16 12:04:37 +0000

    remove undefs

--- cmds/perms.js
+++ cmds/perms.js
@@ -1,27 +1,26 @@
 const { Permissions, MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'perms',
-	category: 'utl',
+	name: "perms",
+	category: "utl",
 	aliases: ["perms", "permissions", "permcheck"],
 	description: "See someone's server permissions",
 	async run(client, message, args) {
 		function format(str) {
 			let newStr = str.replace(/_+/g, " ").toLowerCase();
-			newStr = newStr.split(/ +/).map(x => `${x[0].toUpperCase()}${x.slice(1)}`).join(' ');
+			newStr = newStr.split(/ +/).map(x => `${x[0].toUpperCase()}${x.slice(1)}`).join(" ");
 			return newStr;
 		}
 		const user = await client.config.fetchUser(args[0] || message.author.id);
-		if (!user) user = message.author;
-		const mem = message.guild.members.cache.get(user.id)
+		const mem = message.guild.members.cache.get(user.id);
 		if (!mem) return message.reply("The supplied user is not a member of this server.");
 		const map = Object.keys(Permissions.FLAGS).map(x => mem.hasPermission(x) ? `**${format(x)}**: ${client.config.statics.defaults.emoji.tick}` : `**${format(x)}**: ${client.config.statics.defaults.emoji.err}`).join("\n");
 		message.reply({
 			embed: new MessageEmbed()
-			.setThumbnail(message.guild.iconURL({ dynamic: true }))
-			.setColor(message.author.color)
-			.setTitle(`${user.tag}'s Server Permissions`)
-			.setDescription(map)
-		})
-	}
-}
+				.setThumbnail(message.guild.iconURL({ dynamic: true }))
+				.setColor(message.author.color)
+				.setTitle(`${user.tag}'s Server Permissions`)
+				.setDescription(map),
+		});
+	},
+};
--- cmds/unpunish.js
+++ cmds/unpunish.js
@@ -1,57 +1,59 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-  name: "unpunish",
-  aliases: ["unpunish", "unpnsh"],
-  description: "Remove a user's offence",
-  category: "mod",
-  cst: "tmod",
-  async run(client, message, args) {
-    if (args.length < 2) return message.reply("Correct usage: `" + message.guild.prefix + "unpunish <user> <offence index>`; requires mod")
-    let user = await client.config.fetchUser(args[0]).catch(() => {return;});
-    if (!user) return message.reply(`"${args[0]}" isn't a valid user??`);
-    let ofncs = await client.db.get("ofncs" + user.id) || "0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0";
-        ofncs = ofncs.split(";");
-    for (x in ofncs) {
-      ofncs[x] = Number(ofncs[x])
-    };
-    const index = Number(args[1]);
-    if (!Object.values(client.config.ofncs)[index - 1]) {
-      return message.reply(`Index ${index} out of bounds for length ${Object.keys(client.config.ofncs).length}`)
-  };
+	name: "unpunish",
+	aliases: ["unpunish", "unpnsh"],
+	description: "Remove a user's offence",
+	category: "mod",
+	cst: "tmod",
+	async run(client, message, args) {
+		if (args.length < 2) return message.reply("Correct usage: `" + message.guild.prefix + "unpunish <user> <offence index>`; requires mod");
+		const user = await client.config.fetchUser(args[0]).catch(() => {return;});
+		if (!user) return message.reply(`"${args[0]}" isn't a valid user??`);
+		let ofncs = await client.db.get("ofncs" + user.id) || "0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0";
+		ofncs = ofncs.split(";");
+		for (const x in ofncs) {
+			ofncs[x] = Number(ofncs[x]);
+		}
+		const index = Number(args[1]);
+		if (!Object.values(client.config.ofncs)[index - 1]) {
+			return message.reply(`Index ${index} out of bounds for length ${Object.keys(client.config.ofncs).length}`);
+		}
 
- //   if (typeof ofncs[index - 1] != "number") return message.reply(`Index ${index} out of bounds for length ${ofncs.length}`)
-    ofncs[index - 1] = ofncs[index - 1] - 1;
-    await client.db.set("ofncs" + user.id, ofncs.join(";"));
-    message.reply(`Successfully updated ofncs${user.id} (indx. ${index - 1}) from ${ofncs[index - 1] + 1} to ${ofncs[index - 1]}`)
-    const level = Object.values(client.config.ofncs)[index - 1][1];
-    const mem = await client.guilds.cache.get(client.config.statics.supportServer).members.fetch(user.id)        
-    if (!mem) return;
-    async function unmute() {
-      if (!mem.roles.cache.has(client.config.roles.muted)) return;
-      mem.roles.remove(client.config.roles.muted)
-        .catch(() => {return;});
-      message.reply({
-        embed: new MessageEmbed()
-        .setColor(message.author.color)
-        .setDescription(`${user.tag}'s mute has been removed because of "User unpunished"; they were sent the following message:`)
-      });
-      const e = new MessageEmbed()
-      .setColor(client.config.statics.defaults.colors.green)
-      .setDescription("You have been unmuted. Yay!")
-      .addField("Moderator", message.author.tag)
-      .addField("Reason", "User unpunished by a Moderator.")
-      message.reply(e)
-      user.send({
-        embed: e
-      }).catch(() => {return;});
-    }
-    if (level == 1 && (ofncs[index - 1] < 2)) {
-      await unmute();
-    } else if (level == 2) {
-      await unmute();
-    } else if (level == 3) {
-      await unmute();
-    };
-  },
+		//   if (typeof ofncs[index - 1] != "number") return message.reply(`Index ${index} out of bounds for length ${ofncs.length}`)
+		ofncs[index - 1] = ofncs[index - 1] - 1;
+		await client.db.set("ofncs" + user.id, ofncs.join(";"));
+		message.reply(`Successfully updated ofncs ${user.id} (indx. ${index - 1}) from ${ofncs[index - 1] + 1} to ${ofncs[index - 1]}`);
+		const level = Object.values(client.config.ofncs)[index - 1][1];
+		const mem = await client.guilds.cache.get(client.config.statics.supportServer).members.fetch(user.id);
+		if (!mem) return;
+		async function unmute() {
+			if (!mem.roles.cache.has(client.config.roles.muted)) return;
+			mem.roles.remove(client.config.roles.muted)
+				.catch(() => {return;});
+			message.reply({
+				embed: new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(`${user.tag}'s mute has been removed because of "User unpunished"; they were sent the following message:`),
+			});
+			const e = new MessageEmbed()
+				.setColor(client.config.statics.defaults.colors.green)
+				.setDescription("You have been unmuted. Yay!")
+				.addField("Moderator", message.author.tag)
+				.addField("Reason", "User unpunished by a Moderator.");
+			message.reply(e);
+			user.send({
+				embed: e,
+			}).catch(() => {return;});
+		}
+		if (level == 1 && (ofncs[index - 1] < 2)) {
+			await unmute();
+		}
+		else if (level == 2) {
+			await unmute();
+		}
+		else if (level == 3) {
+			await unmute();
+		}
+	},
 };
