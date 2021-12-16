961101805216ade916a155c0a401e92275be4ac3
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-16 00:13:59 +0000

    Update membercount.js

--- cmds/membercount.js
+++ cmds/membercount.js
@@ -1,19 +1,20 @@
-  const Discord = require("discord.js")
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
 	name: "membercount",
-	aliases: ["members", 'membercount', 'mc'],
+	aliases: ["members", "membercount", "mc"],
 	description: "Gets the total members of a server and seperates them out; bot count, human count, etc, etc.",
-	category: 'utl',
-	usage: 'membercount',
-	async run(client, message, args) {
-		message.reply("", {
-			embed: new Discord.MessageEmbed()
-			.setAuthor(message.guild.name, message.guild.iconURL())
-			.addField("Humans", message.guild.members.cache.filter(e=>!e.user.bot).size, true)
-			.addField("Bots", message.guild.members.cache.filter(e=>e.user.bot).size, true)
-			.addField("Total", message.guild.memberCount, true)
-			.setColor(message.author.color)
-		})
+	category: "utl",
+	usage: "membercount",
+	async run(client, message) {
+		message.reply({
+			embed: new MessageEmbed()
+				.setColor(message.author.color)
+				.setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
+				// the numbers are converted to strings because that is a requirement. An error is thrown otherwise. Probably to enforce consistensy
+				.addField("Humans", message.guild.members.cache.filter((e) => !e.user.bot).size.toString(), true)
+				.addField("Bots", message.guild.members.cache.filter((e) => e.user.bot).size.toString(), true)
+				.addField("Total", message.guild.memberCount.toString(), true),
+		});
 	},
 };
