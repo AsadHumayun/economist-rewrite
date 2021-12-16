829638e417583761f3f4c311b4d41a395821ec80
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-16 00:19:23 +0000

    Update give.js

--- cmds/give.js
+++ cmds/give.js
@@ -1,56 +1,60 @@
-const Discord = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'give',
-	aliases: ['give', 'gv'],
-	description: 'add permissions to users.',
-	category: 'own',
+	name: "give",
+	aliases: ["give", "gv"],
+	description: "add permissions to users.",
+	category: "own",
 	cst: "administrator132465798",
 	async run(client, message, args) {
-		if (args.length < 2) {
-			return message.reply(`You must follow the following format: \`${message.guild.prefix}give <user> <...upgrade>\``);
-		};
+		if (args.length < 2) return message.reply(`You must follow the following format: \`${message.guild.prefix}give <user> <...upgrade>\``);
 		const usr = await client.config.fetchUser(args[0]).catch(() => {return;});
-		if (!usr) return message.reply("Try running the command again, this time actually ping a user llolololololl");
+		if (!usr) return message.reply(`Invalid user "${args[0]}"`, { allowedMentions: { parse: [] } });
 
 		if (!isNaN(args[1])) {
 			let bal = await client.db.get("bal" + usr.id) || "0";
-					bal = Number(bal);
-			let amt = Number(args[1]);
+			bal = Number(bal);
+			const amt = Number(args[1]);
 			bal += amt;
 			await client.db.set("bal" + usr.id, bal);
-			return message.reply({
-				embed: new Discord.MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`:dollar: ${client.config.comma(amt)} have been added to ${usr.tag}'s account`)					
-			});			
-		};
+			return message.channel.send({
+				embeds: [
+					new MessageEmbed()
+						.setColor(message.author.color)
+						.setDescription(`:dollar: ${client.config.comma(amt)} have been added to ${usr.tag}'s account`),
+				],
+			});
+		}
 
-		let val = args.slice(1).join(" ");
+		const val = args.slice(1).join(" ");
 		let cst = await client.db.get("cst" + usr.id);
-				cst = cst ? cst.split(";") : [];
-		const mem = await client.guilds.cache.get(client.config.statics.supportServer).members.fetch(usr.id).catch((err) => {});
-		let role = client.guilds.cache.get(client.config.statics.supportServer).roles.cache.find((f) => f.name.toLowerCase() == val.toLowerCase());
+		cst = cst ? cst.split(";") : [];
+		const mem = await client.guilds.cache.get(client.config.statics.supportServer).members.fetch(usr.id).catch(() => {return;});
+		const role = client.guilds.cache.get(client.config.statics.supportServer).roles.cache.find((f) => f.name.toLowerCase() == val.toLowerCase());
 		if (role) {
 			if (!mem) {
+				// not gonna lie but I have no f****ing clue what this does.
 				let incl = false;
-				for (f of cst) {
+				for (const f of cst) {
 					if (incl == true) return;
 					if (client.guilds.cache.get(client.config.statics.supportServer).roles.cache.get(f)) {
 						if (f == val.toLowerCase()) incl = true;
-					};
-				};
+					}
+				}
 				if (incl == false) cst.push(role.id);
-			} else {
+			}
+			else {
 				mem.roles.add(role.id);
-			};
-		};
+			}
+		}
 		cst.push(val);
-		await client.db.set("cst" + usr.id, cst.join(";"))
-		return message.reply({
-			embed: new Discord.MessageEmbed()
-			.setColor(message.author.color)
-			.setDescription(`${usr.tag} has received ${val}`)
+		await client.db.set("cst" + usr.id, cst.join(";"));
+		return message.channel.send({
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(`${usr.tag} has received ${val}`),
+			],
 		});
 	},
 };
