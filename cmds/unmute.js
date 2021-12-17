da7562395f69b1b5b576cf395e3fb6d21b99f7e4
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-17 21:05:19 +0000

    Update unmute.js

--- cmds/unmute.js
+++ cmds/unmute.js
@@ -11,7 +11,7 @@ module.exports = {
 		if(!usr) return message.reply(`${client.config.statics.defaults.emoji.err} I can't seem to find that user...`);
 		let member = message.guild.member(usr.id);
 		if (!member) return message.reply(`${client.config.statics.defaults.emoji.err} The specified user is not a member of this server`);
-		if (!member.roles.cache.has(client.config.roles.muted)) {
+		if (!member.roles.cache.has(client.config.statics.defaults.roles.muted)) {
 			return message.reply(`${client.config.statics.defaults.emoji.err} ${member.user.tag} isn't muted... how are you gonna unmute them?`);
 		};
 		let dm = new MessageEmbed()
@@ -19,7 +19,7 @@ module.exports = {
 		.setDescription(`Your mute has been removed in ${message.guild.name}`)
 		.addField(`Moderator`, message.author.tag, true)
 		.addField("Reason", args.slice(1).join(' ') || "No reason given", true)
-		await member.roles.remove(client.config.roles.muted);
+		await member.roles.remove(client.config.statics.defaults.roles.muted);
 		await message.reply({
 			embed: new MessageEmbed()
 			.setColor(message.author.color)
