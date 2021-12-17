ec6e470e751635b691b41be74759ccd0e13720c2
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-17 21:15:50 +0000

    Fixed, untested.

--- cmds/reject.js
+++ cmds/reject.js
@@ -1,15 +1,15 @@
-const { MessageEmbed, escapeMarkdown } = require('discord.js');
+const { MessageEmbed, escapeMarkdown } = require("discord.js");
 
 module.exports = {
 	name: "reject",
 	aliases: ["reject", "rej"],
-	description: 'Reject a bug, completely disregarding it.',
+	description: "Reject a bug, completely disregarding it.",
 	dev: true,
-	category: 'btsf',
+	category: "btsf",
 	async run(client, message, args) {
-		if (!args.length) return message.reply("You must provide a bug ID of the bug you wish to reject")
+		if (!args.length) return message.reply("You must provide a bug ID of the bug you wish to reject");
 		const id = args[0];
-		const reason = args.slice(1).join(' ');
+		const reason = args.slice(1).join(" ");
 		message.delete().catch(() => {return;});
 		const val = await client.db.get(`bugr${id}`);
 		if (!val) return message.reply(`${client.config.statics.defaults.emoji.err} No bug report with ID "${id}" was found.`);
@@ -18,20 +18,23 @@ module.exports = {
 				limit: 1,
 				around: val.msg,
 			})
-				.then(async(col) => {
-					const rec = new MessageEmbed()
-						.setColor(client.config.colors.red)
-						.setTitle(val.title)
-						.setDescription(`${client.config.statics.defaults.emoji.err} **Bug report #${val.number} was rejected by ${escapeMarkdown(message.author.tag)}**`)
-						col.first().edit('', {
-							embed: rec,
-						});
-				})
-				.catch((x) => message.reply("There was an error: `" + x + "`"));
-			message.reply(`${client.config.statics.defaults.emoji.err} You've rejected bug with ID **${id}**`);
+			.then(async (col) => {
+				const rec = new MessageEmbed()
+					.setColor(client.config.statics.defaults.channels.colors.red)
+					.setTitle(val.title)
+					.setDescription(`${client.config.statics.defaults.emoji.err} **Bug report #${val.number} was rejected by ${escapeMarkdown(message.author.tag)}**`);
+				col.first().edit({
+					content: null,
+					embeds: [
+						rec,
+					],
+				});
+			})
+			.catch((x) => message.reply("There was an error: `" + x + "`"));
+		message.reply(`${client.config.statics.defaults.emoji.err} You've rejected bug with ID **${id}**`);
 		await client.db.delete(`bugr${id}`);
 		client.users.cache.get(val.author)
 			.send(`Your bug (${id}) has been rejected by ${message.author.tag}\n${reason ? `${message.author.tag}'s Comments: ${reason}` : ""}`)
-				.catch(() => {return;});
+			.catch(() => {return;});
 	},
 };
