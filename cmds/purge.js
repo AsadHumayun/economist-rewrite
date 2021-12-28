d40d7b76a55cb86a3039ff383a6bf5eb27861b74
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-28 10:55:44 +0000

    Transfer old purge to purgemessage; purge edited so it now purges all users' CST values of a specific CST.

--- cmds/purge.js
+++ cmds/purge.js
@@ -1,71 +1,48 @@
 const { MessageEmbed } = require("discord.js");
-const moment = require("moment");
+const { Op } = require("sequelize");
 
 module.exports = {
 	name: "purge",
-	aliases: ["purge", "clear"],
-	description: "mass deletes messages within a channel - max: 100, min: 1",
-	category: "mod",
-	cst: "moderator",
+	aliases: ["purge"],
+	description: "Removes a CST value from every user. This is done db-side, so even if a user shares no mutual servers with the bot, their data will still be edited.",
+	category: "own",
+	cst: "administrator132465798",
 	async run(client, message, args) {
-		let cst = await client.db.get("cst" + message.author.id) || "";
-		cst = cst.split(";");
-		if (message.guild.id != client.config.statics.supportServer && (!cst.includes("purge"))) return;
-		function date(_date = Date.now()) {
-			return moment(_date).format("MMMM Do YYYY, h:mm:ss A");
-		}
-		// not gonna lie, but i just stole this function off the internet
-		// I didn't want to spend too much time on this so I just got a solution and moved on. xD.
-		// I did this so long ago, I don't remember the source, all I know is that it was on stackoverflow.
-		async function clear(n = 1, logs, trigger) {
-			const { channel: source, author } = trigger;
-			if (n < 1 || !logs || !source) return;
-
-			const coll = await source.messages.fetch({ limit: n }),
-				arr = [...coll.values()],
-				collected = [],
-				embeds = [];
-
-			let index = 0;
-			for (let i = 0; i < arr.length; i += 25) {
-				collected.push([]);
-				for (let m = i; m < i + 25; m++) {if (arr[m]) collected[index].push(arr[m]);}
-				index++;
+		if (!args.length || !args.join(" ").replace(/;+/g, "").trim().length) return message.reply("You must specify a CST to purge in order for this command to work!\nNote that semicolons will be removed from your purge argument.");
+		const arg = args.join(" ").replace(/;+/g, "").trim();
+		message.reply({
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(`Purging ${arg}...`),
+			],
+		});
+		client.db.USERS.findAndCountAll({ attributes: ["id", "cst"], where: { cst: { [Op.like]: `%${arg}%` } } }).then((data) => {
+			let updated = 0;
+			for (const value of data.rows) {
+				if (data.rows[data.rows.indexOf(value)].dataValues.cst.split(";").includes(arg)) {
+					data.rows[data.rows.indexOf(value)].dataValues.cst = data.rows[data.rows.indexOf(value)].dataValues.cst.split(";").filter((f) => f.toLowerCase() != arg.toLowerCase()).join(";");
+					updated++;
+				}
 			}
-
-			for (let i = 0; i < collected.length; i++) {
-				const embed = new MessageEmbed()
+			Promise.all(
+				data.rows.map((row) => {
+					client.db.USERS.update({
+						cst: row.dataValues.cst,
+					}, {
+						where: {
+							id: row.dataValues.id,
+						},
+					});
+				}),
+			);
+			message.channel.send({
+				embeds: [
+					new MessageEmbed()
 						.setColor(message.author.color)
-						.setTitle(`Channel Purging${collected.length > 1 ? ` - Part ${i + 1}` : ""}`)
-						.setDescription(`Deleted from: ${source}`)
-						.setAuthor(`${author.tag} (${author.id})`, author.displayAvatarURL({ dynamic: true }))
-						.setTimestamp(trigger.editedAt ? trigger.editedAt : trigger.createdAt),
-					group = collected[i];
-				for (const msg of group) {
-					const a = `${msg.author.tag} (${msg.author.id}) at ${msg.editedAt ? date(msg.editedAt) : date(msg.createdAt)}`;
-					if (!msg.content) msg.content = msg.embeds[0].description || "[]";
-					const c = msg.content.length > 1024 ? msg.content.substring(0, msg.content.length - 3) + "..." : msg.content;
-					embed.addField(a, c);
-				}
-				embeds.push(embed);
-			}
-
-			source.bulkDelete(coll, true).then(async () => {
-				if (message.guild.id == client.config.statics.supportServer) {
-					for (const embed of embeds) await logs.send({ embeds: [embed] });
-				}
-				// can't message.reply, since the original command message will have been deleted.
-				message.channel.send({
-					embeds: [
-						new MessageEmbed()
-							.setColor(message.author.color)
-							.setDescription(`Successfully deleted **${coll.size}** messages in ${Date.now() - message.createdTimestamp} ms`),
-					],
-				});
-			}).catch((err) => message.reply("Sorry, but there was an error whilst performing the request: " + err));
-		}
-		if (isNaN(args[0]) || (!args[0])) return message.reply({ content: `Invalid parameter "${args[0] || "null"}"; "n" must be of type Number`, allowedMentions: { parse: [] } });
-		const logs = client.channels.cache.get(client.config.statics.defaults.channels.msgLogs);
-		clear(Number(args[0]), logs, message);
+						.setDescription(`Received ${data.count} accounts, edited ${updated} accounts`),
+				],
+			});
+		});
 	},
 };
