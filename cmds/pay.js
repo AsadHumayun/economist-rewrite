201d91e6706067c7e38603644f07b4c3e654318c
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-31 00:05:25 +0000

    Fix pay (todo: add 10% tax for payments > 10k)

--- cmds/pay.js
+++ cmds/pay.js
@@ -4,41 +4,46 @@ module.exports = {
 	name: "pay",
 	aliases: ["pay"],
 	category: "ecn",
-	description: "Pay someone else :dollar:\n\nTo pay someone your entire balance, use `all`",
+	description: "Pay someone else :dollar:\n\nTo pay someone your entire balance, use `all`. To pay someone half your balance, use `half`. These keywords are specific to this command only, and will **not** work while using others.",
 	usage: "pay <user> <amount>",
 	async run(client, message, args) {
 		if (args.length < 1) return message.reply("You must mention a user in order for this command to work!");
-		/**
-		 * Tells the `user` they don't have enough money t pay someone else.
-		 */
 		function notEnough() {
-			return message.reply("You don't have enough :dollar: in your bank!");
+			return message.reply("That amount exceeds your current balance");
 		}
-		let authorBal = await client.db.get("bal" + message.author.id) || 0;
-		authorBal = Number(authorBal);
+		const authorBal = isNaN(message.author.data.get("bal")) ? 0 : message.author.data.get("bal");
 		const usr = await client.config.fetchUser(args[0]);
 		if (!usr) return message.reply(`Invalid user "${args[0]}"`, { allowedMentions: { parse: [] } });
+		const data = await client.db.getUserData(usr.id);
 		if (message.author.id == usr.id) return message.reply("You can't pay yourself!");
-		let amt = isNaN(amt) ? 1 : args[1].toLowerCase();
+		let amt = isNaN(args[1]) ? 1 : args[1].toLowerCase();
 		if (amt.toString().startsWith("all")) amt = authorBal;
 		if (amt.toString().startsWith("half")) amt = authorBal / 2;
-		amt = Number(amt);
 		amt = Math.trunc(amt);
-		if (amt < 1) return message.reply("You must enter a positive number.");
-		if (isNaN(amt) && (!amt.startsWith("all") || !amt.startsWith("half"))) return message.reply("You must provide a valid number! (or just `all`|`half`)");
-		if (authorBal < 0 || (!authorBal) || (Number(authorBal - amt) < 0)) return notEnough();
-		const amountLeft = authorBal - amt;
-		if (amountLeft < 0) return notEnough();
-		await client.db.set("bal" + message.author.id, amountLeft);
-		let oldBal = message.author.data.get("bal") || 0;
-		oldBal = Number(oldBal);
-		const newBal = Number(oldBal + amt);
-		await client.db.set("bal" + usr.id, newBal);
+		if (amt < 1) return message.reply("You must enter a positive number");
+		if (isNaN(amt) && (!amt.startsWith("all") || !amt.startsWith("half"))) return message.reply("You must provide a valid number! (or just `all` or `half`)");
+		if (authorBal < 0 || (authorBal - amt < 0)) return notEnough();
+		if (authorBal - amt < 0) return notEnough();
+		const oldBal = isNaN(data.get("bal")) ? 0 : data.get("bal");
+		await client.db.USERS.update({
+			bal: authorBal - amt,
+		}, {
+			where: {
+				id: message.author.id,
+			},
+		});
+		await client.db.USERS.update({
+			bal: oldBal + amt,
+		}, {
+			where: {
+				id: usr.id,
+			},
+		});
 		message.reply({
 			embeds: [
 				new MessageEmbed()
 					.setColor(message.author.color)
-					.setDescription(`${message.author.tag} has paid :dollar: ${client.config.comma(amt)} (${amt.toString().length} digits) into ${usr.tag}'s account`),
+					.setDescription(`:dollar: ${client.config.trim(client.config.comma(client.config.noExponents(amt)), 1000)} have been transferred to ${usr.tag}'s account`),
 			],
 		});
 	},
