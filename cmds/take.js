dca0c0401b911799b283f0e5c8fc6114099b4521
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-31 18:58:33 +0000

    Fix take

--- cmds/take.js
+++ cmds/take.js
@@ -1,51 +1,66 @@
-const Discord = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'take',
-	aliases: ['take'],
-	description: 'removes permissions from users.',
-	category: 'own',
+	name: "take",
+	aliases: ["take"],
+	description: "removes permissions from users.",
+	category: "own",
 	cst: "administrator132465798",
 	async run(client, message, args) {
 		if (args.length < 2) {
 			return message.reply(`You must follow the following format: \`${message.guild.prefix}take <user> <...upgrade>\``);
-		};
-		const permission = args.slice(1).join(' ');
+		}
+		const permission = args.slice(1).join(" ");
 		const usr = await client.config.fetchUser(args[0]).catch(() => {return;});
 		if (!usr) return message.reply("Try running the command again, this time actually ping a user llolololololl");
-
+		const data = await client.db.getUserData(usr.id);
 		if (!isNaN(args[1])) {
-			let bal = message.author.data.get("bal") || "0";
-				bal = Number(bal);
-			let amt = Number(args[1]);
-				bal -= amt;
-			await client.db.set("bal" + usr.id, bal);
+			let bal = data.get("bal") || 0;
+			const amt = Number(args[1]);
+			bal -= amt;
+			await client.db.USERS.update({
+				bal,
+			}, {
+				where: {
+					id: usr.id,
+				},
+			});
 			return message.reply({
-				embed: new Discord.MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`:dollar: ${client.config.comma(amt)} have been removed from ${usr.tag}'s account`)					
-			});			
-		};
+				embeds: [
+					new MessageEmbed()
+						.setColor(message.author.color)
+						.setDescription(`:dollar: ${client.config.comma(amt)} have been removed from ${usr.tag}'s account`),
+				],
+			});
+		}
 
-		let cst = await client.db.get("cst" + usr.id);
-			cst = cst ? cst.split(";") : [];
+		let cst = data.get("cst") ? data.get("cst").split(";") : [];
 		const mem = await client.guilds.cache.get(client.config.statics.supportServer).members.fetch(usr.id).catch(() => {return;});
-		let role = client.guilds.cache.get(client.config.statics.supportServer).roles.cache.find((r) => r.name.toLowerCase() == permission.toLowerCase());
-		if (mem && (role)) {
-			mem.roles.remove(role.id);
-		} else if (role) {
-			for (f of cst) {
+		const role = client.guilds.cache.get(client.config.statics.supportServer).roles.cache.find((r) => r.name.toLowerCase() == permission.toLowerCase());
+		if (mem && role) {
+			mem.roles.remove(role.id).catch(() => {return;});
+		}
+		else if (role) {
+			for (const f of cst) {
 				if (client.guilds.cache.get(client.config.statics.supportServer).roles.cache.get(f) && (f == role.id)) {
 					cst = cst.filter((x) => x != f);
-				};
-			};
-		};
+				}
+			}
+		}
 		cst = cst.filter(x => ![permission].includes(x)).join(";");
-		await client.db.set("cst" + usr.id, cst);
-		return message.reply({
-			embed: new Discord.MessageEmbed()
-			.setDescription(`${usr.tag} has lost ${permission}`)
-			.setColor(message.author.color)
+		await client.db.USERS.update({
+			cst,
+		}, {
+			where: {
+				id: usr.id,
+			},
+		});
+		message.reply({
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(`${usr.tag} has lost ${permission}`),
+			],
 		});
 	},
 };
