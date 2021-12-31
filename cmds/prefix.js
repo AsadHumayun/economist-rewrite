877066642a5397ff852506813d4bfd4368b426dc
Static <49240408+almostStatic@users.noreply.github.com>
2021-12-31 18:13:04 +0000

    Fix: prefix

--- cmds/prefix.js
+++ cmds/prefix.js
@@ -8,8 +8,14 @@ module.exports = {
 	async run(client, message, args) {
 		if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) return message.reply("You need the `MANAGE_GUILD` permission in order to use this command!");
 		const prefix = args[0].trim();
-		if (!prefix || prefix.length > 3) return message.reply(`Invalid argument "${args[0]}"; this may not exceed 3 characters in length.`, { alloweMentions: { parse: [] } });
-		await client.db.set(`prefix${message.guild.id}`, prefix.toLowerCase());
+		if (!prefix || prefix.length > 3) return message.reply({ content: `Invalid prefix "${args[0]}"`, alloweMentions: { parse: [] } });
+		await client.db.GUILDS.update({
+			prefix,
+		}, {
+			where: {
+				id: message.guild.id,
+			},
+		});
 		message.reply({
 			embeds: [
 				new MessageEmbed()
