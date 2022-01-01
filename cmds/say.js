3d62bb752ca06ead61375178ba587be150903b54
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-01 16:22:01 +0000

    Fix say

--- cmds/say.js
+++ cmds/say.js
@@ -1,14 +1,14 @@
-const { MessageEmbed } = require('discord.js')
-
 module.exports = {
-	name: 'say',
-	aliases: ['say', 'echo'],
-	description: 'Gets the bot to say your message',
-	category: 'fun',
+	name: "say",
+	aliases: ["say", "echo"],
+	description: "Gets the bot to say your message",
+	category: "fun",
 	cst: "say",
+	cstMessage: "This command is locked due to the possibility of abuse",
 	async run(c, message, a) {
 		if (!a.length) return message.reply("You must specify a message for me to say!");
-		var msg = a.join(' ');
-		message.reply(msg);
+		const msg = a.join(" ");
+		message.delete();
+		message.channel.send({ content: msg, allowedMentions: { parse: [] } });
 	},
 };
