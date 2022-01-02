478337148b7efae1b16ea6dfb5aa290e0c1f6d4f
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-02 02:18:28 +0000

    + customValidators
    this now means no one can do ~Set user bal <NaN> - throws an error, same for other keys.

--- models/User.js
+++ models/User.js
@@ -1,3 +1,11 @@
+const customValidators = {
+	allNumbers: function(data) {
+		data = data.toString().split(";");
+		data = data.map(Number);
+		if (data.includes(NaN)) throw new Error("[NumberArray]: All values must be numeric.");
+	},
+};
+
 module.exports = (sequelize, DataTypes) => {
 	return sequelize.define("user", {
 		// id: Discord user ID - provided by Discord API
@@ -6,37 +14,67 @@ module.exports = (sequelize, DataTypes) => {
 			type: DataTypes.STRING,
 			allowNull: false,
 			unique: true,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		adren: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		adrenc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		bal: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		bcmd: {
 			type: DataTypes.TEXT,
 		},
 		bgc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		bio: {
-			type: DataTypes.STRING,
+			type: DataTypes.STRING("2000"),
 			defaultValue: "User has not set a bio.",
+			validate: {
+				len: [0, 1500],
+			},
 		},
 		cfc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		chillc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		chillpills: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		chnl: {
 			type: DataTypes.TEXT,
+			validate: {
+				isAllNumbers: customValidators.allNumbers,
+			},
 		},
 		clr: {
 			type: DataTypes.STRING,
@@ -44,6 +82,9 @@ module.exports = (sequelize, DataTypes) => {
 		},
 		cmds: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		cst: {
 			type: DataTypes.TEXT,
@@ -53,43 +94,82 @@ module.exports = (sequelize, DataTypes) => {
 		},
 		crls: {
 			type: DataTypes.STRING,
-			defaultValue: "default",
+			defaultValue: "dragon",
+			validate: {
+				isAlphanumeric: true,
+			},
 		},
 		dgrc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		dlc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		dlstr: {
 			type: DataTypes.STRING,
+			validate: {
+				isAlphanumeric: true,
+			},
 		},
 		dns: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		dose0: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		dose1: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		dpc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		drgs: {
 			type: DataTypes.TEXT,
+			validate: {
+				isAllNumbers: customValidators.allNumbers,
+			},
 		},
 		fdc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		fishc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		fsh: {
 			type: DataTypes.STRING,
+			validate: {
+				isAllNumbers: customValidators.allNumbers,
+			},
 		},
 		hgs: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		mt: {
 			type: DataTypes.STRING,
@@ -99,14 +179,23 @@ module.exports = (sequelize, DataTypes) => {
 		},
 		ofncs: {
 			type: DataTypes.STRING,
+			validate: {
+				isAllNumbers: customValidators.allNumbers,
+			},
 		},
 		pet: {
 			type: DataTypes.STRING,
 			defaultValue: "1;10000;100;0;1;1;1;1;0;1",
 			allowNull: false,
+			validate: {
+				isAllNumbers: customValidators.allNumbers,
+			},
 		},
 		petbu: {
 			type: DataTypes.STRING,
+			validate: {
+				isAllNumbers: customValidators.allNumbers,
+			},
 		},
 		petname: {
 			type: DataTypes.STRING(128),
@@ -114,6 +203,9 @@ module.exports = (sequelize, DataTypes) => {
 		},
 		pq: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		qts: {
 			type: DataTypes.TEXT,
@@ -121,31 +213,51 @@ module.exports = (sequelize, DataTypes) => {
 		replacers: {
 			type: DataTypes.TEXT,
 		},
-		robc: {
+		rbc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		sgstc: {
 			type: DataTypes.DECIMAL,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		spse: {
 			type: DataTypes.STRING,
 			unique: true,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		sntc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		srchc: {
 			type: DataTypes.DECIMAL,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		stn: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		stnb: {
 			type: DataTypes.STRING(128),
-			defaultValue: "stunned",
 		},
 		strc: {
 			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 		upgr: {
 			type: DataTypes.TEXT,
@@ -153,6 +265,9 @@ module.exports = (sequelize, DataTypes) => {
 		v: {
 			type: DataTypes.STRING,
 			defaultValue: "1;0",
+			validate: {
+				isAllNumbers: customValidators.allNumbers,
+			},
 		},
 		wl: {
 			type: DataTypes.STRING,
@@ -160,9 +275,15 @@ module.exports = (sequelize, DataTypes) => {
 		xp: {
 			type: DataTypes.STRING,
 			defaultValue: "1;0",
+			validate: {
+				isAllNumbers: customValidators.allNumbers,
+			},
 		},
 		xpc: {
-			type: DataTypes.DECIMAL,
+			type: DataTypes.NUMBER,
+			validate: {
+				isNumeric: true,
+			},
 		},
 	});
 };
