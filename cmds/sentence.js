8dead491ce43c74d45860ccfb7133dfae5d0468f
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-03 06:53:10 +0000

    Fix sentence (ties heavily with commit on config.js)

--- cmds/sentence.js
+++ cmds/sentence.js
@@ -1,10 +1,12 @@
-const { MessageEmbed, escapeMarkdown } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const delay = require("delay");
 
 module.exports = {
+	_msgSendDelay: 1000,
 	name: "sentence",
 	aliases: ["sentence", "sente"],
 	cst: "judge",
+	cstMessage: "You must be a judge in order to use this command!",
 	category: "ecn",
 	description: "judge a user, stunning them in a range of 4-10 minutes.",
 	async run(client, message, args) {
@@ -15,35 +17,75 @@ module.exports = {
 				return message.reply(`You must wait ${data} before you can sentence again!`);
 			}
 		}
-		async function dm(user, userColor, txt) {
-			const embed = new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(txt);
-			await client.users.cache.get(user.id)
-				.send({ embed: new MessageEmbed(embed).setColor(userColor) })
-				.catch(() => {return;});
-			message.reply({ embed });
-			await delay(1000);
-		}
-		if (!args.length) return message.reply("You need to ping someone to sentence, dum dum");
+		if (!args.length) return message.reply("You must mention your target in order for this command to work!");
 		const user = await client.config.fetchUser(args[0]).catch(() => {return;});
-		if (!user) return message.reply("You need to ping someone to sentence, dum dum");
-		await client.db.set("sntc" + message.author.id, client.config.parseCd(message.createdTimestamp, ms("6h")));
-		let usercolor = await client.db.get("clr" + user.id) || client.config.defaultHexColor;
-		usercolor = usercolor.split(";")[0];
+		if (!user) return message.reply({ content: `Invalid user "${args[0]}"`, allowedMentions: { parse: [] } });
+		const data = await client.db.getUserData(user.id);
+		await client.db.USERS.update({
+			// 21600000ms = 6h
+			sntc: client.config.parseCd(message.createdTimestamp, 21600000),
+		}, {
+			where: {
+				id: message.author.id,
+			},
+		});
 		const didntWork = Math.floor(Math.random() * 100);
 
-		let bal = await client.db.get("bal" + user.id) || 0;
-		bal = Number(bal);
-		let amtLost = bal / 5;
+		const bal = data.get("bal") || 0;
+		let amtLost = Math.floor(bal / 5);
 		if (bal - amtLost < 0) amtLost = bal;
-		amtLost = Math.floor(amtLost);
+		//		await client.config.dm()
+		await client.config.dm({
+			userId: user.id,
+			message: {
+				embeds: [
+					new MessageEmbed()
+						.setColor(message.author.color)
+						.setDescription(`${message.author.tag} has summoned ${user.tag} in court`),
+				],
+			},
+			channel: message.channel,
+		});
+		await delay(this._msgSendDelay);
+		await client.config.dm({
+			userId: user.id,
+			message: {
+				embeds: [
+					new MessageEmbed()
+						.setColor(message.author.color)
+						.setDescription(`It turns out ${user.tag} is a loser and ends up pissing on the floor, losing their dignity`),
+				],
+			},
+			channel: message.channel,
+		});
+		await delay(this._msgSendDelay);
 
-		await dm(user, usercolor, `${message.author.tag} has summoned ${user.tag} in court`);
-		await dm(user, usercolor, `It turns out ${user.tag} is a loser and ends up pissing on the floor, losing their dignity`);
 		if (didntWork > 90) {
-			await dm(user, usercolor, `${user.tag}'s lawyer was able to save ${user.tag}'s ass this time round!`);
-			await dm(user, usercolor, `:skull_crossbones: ${message.author.tag} failed to sentence ${user.tag}`);
+			// 10% chance doesn't work, I believe.
+			await client.config.dm({
+				userId: user.id,
+				message: {
+					embeds: [
+						new MessageEmbed()
+							.setColor(message.author.color)
+							.setDescription(`${user.tag}'s lawyer was able to save ${user.tag}'s ass this time round!`),
+					],
+				},
+				channel: message.channel,
+			});
+			await delay(this._msgSendDelay);
+
+			await client.config.dm({
+				userId: user.id,
+				message: {
+					embeds: [
+						new MessageEmbed()
+							.setColor(message.author.color)
+							.setDescription(`:slight_frown: ${message.author.tag} failed to sentence ${user.tag}`),
+					],
+				},
+				channel: message.channel,
+			});
 			return;
 		}
 		else {
@@ -51,16 +93,60 @@ module.exports = {
 			if (stunTime < 4) stunTime = 4;
 			stunTime *= 60_000;
 			//		stn: function (id, amt, client) {
-			await client.stn(user.id, stunTime / 60_000, client);
-			await client.db.set("stnb" + user.id, "in jail");
-			await dm(user, usercolor, `After careful consideration, it is decided that ${user.tag} is punishable as a result of their insane ugliness; ${message.author.tag} has won the court case`);
-			await dm(user, usercolor, `:dollar: ${client.config.comma(amtLost) || "0"} have been moved to ${message.author.tag}'s account since ${user.tag} was unable to win the court case lol`);
-			await client.db.set("bal" + user.id, bal - amtLost);
-			let oldBal = await client.db.get("bal" + message.author.id) || 0;
-			oldBal = Number(oldBal);
-			const newBal = (oldBal + amtLost);
-			await client.db.set("bal" + message.author.id, newBal);
-			await dm(user, usercolor, `${user.tag} has been put into jail for ${stunTime / 60_000} minutes`);
+			await client.config.stn({
+				userId: user.id,
+				minutes: Math.trunc(stunTime / 60_000),
+				stnb: "in jail",
+			});
+			await client.config.dm({
+				userId: user.id,
+				message: {
+					embeds: [
+						new MessageEmbed()
+							.setColor(message.author.color)
+							.setDescription(`After careful consideration, it is decided that ${user.tag} is punishable as a result of their insane ugliness; ${message.author.tag} has won the court case`),
+					],
+				},
+				channel: message.channel,
+			});
+			await delay(this._msgSendDelay);
+
+			await client.config.dm({
+				userId: user.id,
+				message: {
+					embeds: [
+						new MessageEmbed()
+							.setColor(message.author.color)
+							.setDescription(`:dollar: ${client.config.comma(amtLost) || "0"} have been moved to ${message.author.tag}'s account since ${user.tag} was unable to win the court case lol`),
+					],
+				},
+				channel: message.channel,
+			});
+			await client.db.USERS.update({
+				bal: bal - amtLost,
+			}, {
+				where: {
+					id: user.id,
+				},
+			});
+			await client.db.USERS.update({
+				bal: bal + amtLost,
+			}, {
+				where: {
+					id: message.author.id,
+				},
+			});
+			await client.config.dm({
+				userId: user.id,
+				message: {
+					embeds: [
+						new MessageEmbed()
+							.setColor(message.author.color)
+							.setDescription(`${user.tag} has been put into jail for ${stunTime / 60_000} minutes`),
+					],
+				},
+				channel: message.channel,
+			});
 		}
 	},
 };
