9b4823ae3658232813cda0611f345e2941585b67
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-03 01:25:27 +0000

    Fix unpunish + allow mods to specify reason for unpunish

--- cmds/unpunish.js
+++ cmds/unpunish.js
@@ -7,22 +7,29 @@ module.exports = {
 	category: "mod",
 	cst: "moderator",
 	async run(client, message, args) {
-		if (args.length < 2) return message.reply("Correct usage: `" + message.guild.prefix + "unpunish <user> <offence index>`; requires mod");
+		if (args.length < 2) return message.reply("Correct usage: `" + message.guild.prefix + "unpunish <user> <offence index> <?reason>`; requires mod");
 		const user = await client.config.fetchUser(args[0]).catch(() => {return;});
-		if (!user) return message.reply(`"${args[0]}" isn't a valid user??`);
-		let ofncs = await client.db.get("ofncs" + user.id);
-		ofncs = ofncs ? ofncs.split(";") : [];
-		ofncs = ofncs.map(Number);
+		if (!user) return message.reply({ content: `Invalid user "${args[0]}"`, allowedMentions: { parse: [] } });
+		const data = await client.db.getUserData(user.id);
+		const ofncs = data.get("ofncs") ? data.get("ofncs").split(";").map(Number) : [];
+		const reason = args.slice(2).join(" ");
 		const index = Number(args[1]);
 		if (!Object.values(client.config.statics.defaults.ofncs)[index - 1]) {
 			return message.reply(`Index ${index} out of bounds for length ${Object.keys(client.config.statics.defaults.ofncs).length}`);
 		}
+		if (!ofncs[index - 1]) ofncs[index - 1] = 0;
 		ofncs[index - 1] = ofncs[index - 1] - 1;
 		while (ofncs[ofncs.length - 1] == 0) {
 			ofncs.pop();
 		}
-		await client.db.set("ofncs" + user.id, ofncs.join(";"));
-		message.reply(`Successfully updated ofncs ${index} ${user.id} from ${ofncs[index - 1] + 1} to ${ofncs[index - 1]}`);
+		await client.db.USERS.update({
+			ofncs: ofncs.join(";"),
+		}, {
+			where: {
+				id: user.id,
+			},
+		});
+		message.reply(`Successfully updated ofncs ${index} ${user.id} from ${ofncs[index - 1] + 1} to ${ofncs[index - 1]} ${reason ? `(reason: ${reason})` : ""}`);
 		const level = Object.values(client.config.statics.defaults.ofncs)[index - 1][1];
 		const mem = await client.guilds.cache.get(client.config.statics.supportServer).members.fetch(user.id);
 		if (!mem) return;
@@ -33,28 +40,28 @@ module.exports = {
 				embeds: [
 					new MessageEmbed()
 						.setColor(message.author.color)
-						.setDescription(`${user.tag}'s mute has been removed because of "User unpunished"; they were sent the following message:`),
+						.setDescription(`${user.tag}'s mute has been removed because of "${reason || "user unpunished"}"; they were sent the following message:`),
 				],
 			});
 			const e = new MessageEmbed()
 				.setColor(client.config.statics.defaults.colors.green)
 				.setDescription("Your mute has been removed")
 				.addField("Moderator", message.author.tag)
-				.addField("Reason", "User unpunished by a Moderator.");
-			message.channel.send({ embeds: [e] });
-			user.send({ embeds: [e] }).catch(() => {return;});
+				.addField("Reason", reason.toString() || "Moderator didn't specify a reason.");
+			message.channel.send({ embeds: [ e ] });
+			user.send({ embeds: [ e ] }).catch(() => {return;});
 		}
 		if (level == 1 && (ofncs[index - 1] < 2)) {
-			await unmute();
+			unmute();
 		}
 		else if (level == 2) {
-			await unmute();
+			unmute();
 		}
 		else if (level == 3) {
-			await unmute();
+			unmute();
 		}
 		else if (level == 4) {
-			await unmute();
+			unmute();
 		}
 	},
 };
