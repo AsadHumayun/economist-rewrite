d86b64c8fc741088dc15ba61ece7a2ba1094458c
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-03 01:25:44 +0000

    Fix tame

--- cmds/tame.js
+++ cmds/tame.js
@@ -1,21 +1,27 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'tame',
-	aliases: ['tame', 'getdragon'],
-	category: 'pet',	
-	description: 'tame a dragon! (`~dragon`)',
-	async run(client, message, args) {
-		let cst = await client.db.get("cst" + message.author.id);
-				cst = cst ? cst.split(";") : [];
+	name: "tame",
+	aliases: ["tame", "getdragon"],
+	category: "pet",
+	description: "tame a dragon! (`~dragon`)",
+	async run(client, message) {
+		const cst = message.author.data.get("cst") ? message.author.data.get("cst").split(";") : [];
 		if (cst.includes("dragon")) return message.reply("You already seem to own a dragon!");
 		cst.push("dragon");
-		await client.db.set("cst" + message.author.id, cst.join(";"));
-		await client.db.set('pet' + message.author.id, client.config.statics.defaults.dragon);
+		await client.db.USERS.update({
+			cst: cst.join(";"),
+		}, {
+			where: {
+				id: message.author.id,
+			},
+		});
 		message.reply({
-			embed: new MessageEmbed()
-			.setColor(message.author.color)
-			.setDescription(`${message.author.tag} has successfully tameed a :dragon_face: dragon! | \`${message.guild.prefix}dragon\``)
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(`${message.author.tag} has successfully tameed a :dragon_face: dragon! | \`${message.guild.prefix}dragon\``),
+			],
 		});
 	},
 };
