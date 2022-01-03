f506d4164e207a874bc302ba65607e18d0d73a37
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-03 07:10:04 +0000

    move customValidators to their own file

--- models/User.js
+++ models/User.js
@@ -1,16 +1,4 @@
-const customValidators = {
-	allNumbers: function(data) {
-		data = data.toString().split(";").map(Number);
-		if (data.includes(NaN)) throw new Error("\n[CustomValidator => allNumbers] [Fail] All values must be numeric.");
-	},
-	isNumericString: function(data) {
-		if (typeof data !== "string" || isNaN(data)) throw new Error(`\n[CustomValidator => isNumericString] [Fail] ID must be a numeric string. Received type: ${typeof data} (NaN?: ${isNaN(data)})`);
-	},
-	// had to be introduced because Sequelize didn't recognise standard form numbers as numbers :shrug:
-	isNumber: function(data) {
-		if (isNaN(Number(data))) throw new Error(`\n[CustomValidator => isNumber] [Fail] Value must be a valid JavaScript number. Received type: ${typeof data}`);
-	},
-};
+const customValidators = require("./_customValidators");
 
 module.exports = (sequelize, DataTypes) => {
 	return sequelize.define("user", {
---
+++ models/_customValidators.js
@@ -0,0 +1,13 @@
+module.exports = {
+	allNumbers: function(data) {
+		data = data.toString().split(";").map(Number);
+		if (data.includes(NaN)) throw new Error("\n[CustomValidator => allNumbers] [Fail] All values must be numeric.");
+	},
+	isNumericString: function(data) {
+		if (typeof data !== "string" || isNaN(data)) throw new Error(`\n[CustomValidator => isNumericString] [Fail] ID must be a numeric string. Received type: ${typeof data} (NaN?: ${isNaN(data)})`);
+	},
+	// had to be introduced because Sequelize didn't recognise standard form numbers as numbers :shrug:
+	isNumber: function(data) {
+		if (isNaN(Number(data))) throw new Error(`\n[CustomValidator => isNumber] [Fail] Value must be a valid JavaScript number. Received type: ${typeof data}`);
+	},
+};
