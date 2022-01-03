979ef9335fa0c05c632ec46b1e8f97b76567e820
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-03 01:25:03 +0000

    Fix unban

--- cmds/unban.js
+++ cmds/unban.js
@@ -1,34 +1,34 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Permissions } = require("discord.js");
 
 module.exports = {
-	"name": "unban",
-	"aliases": ["unban"],
-	"description": "unbans a user from the current guild.",
-	"category": "mod",
+	name: "unban",
+	aliases: ["unban"],
+	description: "unbans a user from the current guild.",
+	category: "mod",
 	cst: "moderator",
+	ssOnly: true,
 	async run(client, message, args) {
-		if (!message.member.roles.cache.has(client.config.statics.defaults.roles.mod.normal)) return message.reply("You must have the Moderator role in order to use this command. Trial Mods do not have permission to use this command, either.");
-
+		if (!message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) return message.reply("You must have the BAN_MEMBERS permission in order to use this command!");
 		if (!args.length) return message.reply("You must follow the format of `" + message.guild.prefix + "unban <user> [reason]`");
-		let user = await client.config.fetchUser(args[0]).catch(() => {return;});
+		const user = await client.config.fetchUser(args[0]).catch(() => {return;});
 		if (!user) return message.reply(`${client.config.statics.defaults.emoji.err} You have provided an invalid user!`);
-		var reason = args.slice(1).join(' ') || "No reason given";
+		const reason = args.slice(1).join(" ") || "No reason given";
 		const Notification = new MessageEmbed()
-		.setColor(client.config.colors['green'])
-		.setDescription(`You have been unbanned from ${message.guild.name}. Please conduct yourself appropriately such that you do not get banned again. [Support Server Invite](${client.config.ssInvite})`)
-		.addField("Moderator", message.author.tag)
-		.addField("Reason", reason)
+			.setColor(client.config.statics.defaults.colors["green"])
+			.setDescription(`You have been unbanned from ${message.guild.name}. Please conduct yourself appropriately, in a manner such that you do not get banned again. [Support Server Invite](${client.config.statics.ssInvite})`)
+			.addField("Moderator", message.author.tag)
+			.addField("Reason", reason);
 
 		const msgs = [`${client.config.statics.defaults.emoji.tick} ${user.tag}'s ban has been removed because of "${reason || "<UNKNOWN REASON>"}"; they have been sent the following message:`];
 		await message.guild.members.unban(user.id);
 		message.reply({
-			embed: new MessageEmbed()
-			.setColor(message.author.color)
-			.setDescription(msgs[0])
-		})
-		message.reply({ embed: Notification });
-		client.users.cache.get(user.id)
-			.send({ embed: Notification })
-				.catch(() => {return;});
-	}
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(msgs[0]),
+				Notification,
+			],
+		});
+		client.users.cache.get(user.id).send({ embed: [ Notification ] }).catch(() => {return;});
+	},
 };
