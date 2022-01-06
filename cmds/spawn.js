adb3490a582e570d043907f3f725a50cbd39bbf5
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-06 23:58:55 +0000

    Fix spawn

--- cmds/spawn.js
+++ cmds/spawn.js
@@ -1,17 +1,25 @@
 "use strict";
-import { MessageEmbed } from "discord.js";
 
 export default {
-	"name": "spawn",
-	"aliases": ["spawn"],
-	"description": "Spawns a briefcase in the current channel",
-	"category": 'own',
-	"cst": "spawn",
-	async run(client, message, args) {
-		let data = await client.db.get(`briefcase${message.channel.id}`);
-		if (data) return message.reply(`${client.config.statics.defaults.emoji.err} There is already a briefcase in this channel! Steal it with \`${message.guild.prefix}steal\``);
-
-		await client.db.set(`briefcase${message.channel.id}`, true);
-		message.reply(`Someone just dropped their :briefcase: briefcase in this channel! Hurry up and steal it with \`${message.guild.prefix}steal\`!`);
-	}
-}
+	name: "spawn",
+	aliases: ["spawn"],
+	description: "Spawns a briefcase in the current channel",
+	category: "own",
+	cst: "spawn",
+	async run(client, message) {
+		const channel = await client.db.CHNL.findOne({ where: { id: message.channel.id } });
+		if (channel.get("pkg")) {
+			return message.reply(`There is already a briefcase in this channel! Steal it with \`${message.guild.prefix}steal\``);
+		}
+		else {
+			message.reply(`Someone just dropped their :briefcase: briefcase in this channel! Hurry up and steal it with \`${message.guild.prefix}steal\`!`);
+			await client.db.CHNL.update({
+				pkg: true,
+			}, {
+				where: {
+					id: message.channel.id,
+				},
+			});
+		}
+	},
+};
