4ffddd6efe4aabc1638553ec3e522dd766660992
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-06 23:58:49 +0000

    Fix spouse

--- cmds/spouse.js
+++ cmds/spouse.js
@@ -3,22 +3,22 @@ import { MessageEmbed } from "discord.js";
 
 export default {
 	name: "spouse",
-	aliases: ['spouse'],
-	category: 'ecn',
+	aliases: ["spouse"],
+	category: "ecn",
 	description: "See who someone is married to",
 	async run(client, message, args) {
 		let usr = await client.config.fetchUser(args[0]).catch(() => {return;});
-		if (!usr) {
-			usr = message.author;
-		};		
-		let spouse = await client.db.get("spouse" + usr.id);
+		if (!usr) usr = message.author;
+		const data = await client.db.getUserData(usr.id);
+		const spouse = data.get("spse");
 		if (!spouse) return message.reply(`${usr.tag} isn't married to anyone. L`);
-		let user = await client.users.fetch(spouse);
-		let tag = `${user.username}#${user.discriminator}`;
+		const user = await client.users.fetch(spouse);
 		message.reply({
-			embed: new MessageEmbed()
-			.setDescription(`:two_hearts: ${usr.tag} is currently married to ${tag}`)
-			.setColor(message.author.color)
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setDescription(`:two_hearts: ${usr.tag} is currently married to ${user.tag}`),
+			],
 		});
 	},
 };
