10c55d8cff279088b02df23d3666e9a063b88976
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-07 11:09:51 +0000

    Fix suggest

--- cmds/suggest.js
+++ cmds/suggest.js
@@ -2,34 +2,38 @@
 import { MessageEmbed } from "discord.js";
 
 export default {
-	name: 'suggest',
-	aliases: ['suggest', 'addsmthnew'],
-	description: 'Suggest a new idea to be added to the bot; will be posted in <#758598514623643690>',
-	category: 'utl',
+	name: "suggest",
+	aliases: ["suggest", "addsmthnew"],
+	description: "Suggest a new idea to be added to the bot; will be posted in <#758598514623643690>",
+	category: "utl",
 	async run(client, message, args) {
-		let cd = await client.db.get("sgstc" + message.author.id);
-		let scnd = client.config.cooldown(message.createdTimestamp, cd*60_000);
-		if (scnd) {
-			return message.reply(`You must wait another ${scnd} before suggesting again`);
-		};
+		const scnd = client.config.cooldown(message.createdTimestamp, (message.author.data.get("sgstc") || 0) * 60_000);
+		if (scnd) return message.reply(`You must wait another ${scnd} before suggesting again`);
 
-		if (!args.length) return message.reply(`You must provide a suggestion for me to uhh... suggest?`)
-		await client.db.set('sgstc' + message.author.id, client.config.parseCd(message.createdTimestamp, 20000, true));				
-		var suggest = args.join(' ');
-			message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`Your suggestion was successfully posted in our [support server](${client.config.ssInvite})`)
-			});
-			client.channels.cache.get(client.config.channels.suggestions)
-				.send({
-					embed: new MessageEmbed()
+		if (!args.length) return message.reply("You must provide a suggestion for this command to work!");
+		await client.db.USERS.update({
+			sgstc: client.config.parseCd(message.createdTimestamp, 20000, true),
+		}, {
+			where: {
+				id: message.author.id,
+			},
+		});
+		const suggest = args.join(" ");
+		message.reply({
+			embeds: [
+				new MessageEmbed()
 					.setColor(message.author.color)
-					.setTitle(`New Suggestion`)
-					.setThumbnail(message.author.displayAvatarURL())
-					.setDescription(suggest)
-					.setFooter(`${message.author.tag} (${message.author.id})`)
-				})
-					.then((m) => { m.react('👍'); m.react('👎') });
-	}
-}
+					.setDescription(`Thanks ${message.author.tag}, your suggestion has been received!`),
+			],
+		});
+		client.channels.cache.get(client.config.statics.defaults.channels.suggestions).send({
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setAuthor({ name: `${message.author.tag} (${message.author.id})`, iconURL: message.author.displayAvatarURL({ dynamic: false }), url: message.url })
+					.setDescription(suggest),
+			],
+		})
+			.then((m) => { m.react("👍"); m.react("👎"); });
+	},
+};
