5d22b87971e82e160f19c2fcd374d1419bf93f79
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-07 14:29:14 +0000

    Fix assign/upgr system

---
+++ cmds/assign.js
@@ -0,0 +1,49 @@
+import { MessageEmbed } from "discord.js";
+
+export default {
+	name: "assign",
+	aliases: ["assign", "assignupgr"],
+	description: "Assigns access to your currently owned or co-owned dragon aliases",
+	category: "pet",
+	async run(client, message, args) {
+		if (args.length < 2) return message.reply(`You msut folloe the format of \`${message.guild?.prefix || "~"}assign <user> <dragon alias | upgrade>\` in order for this command to work!`);
+		const user = await client.config.fetchUser(args[0]).catch(() => {return;});
+		const kw = args[1];
+		if (!user) return message.reply({ content: `Unknown user "${args[0]}"`, allowedMentions: { parse: [] } });
+		let upgr = message.author.data.get("upgr")?.split(";") || [];
+		if (upgr.length < 2) return message.reply("You don't have any assignable dragon aliases!");
+		upgr = client.config.listToMatrix(upgr, 2);
+		const keys = upgr.map((f) => f[0]);
+		if (!keys.includes(kw)) return message.reply({ content: `Unknown upgr "${kw}"; \`${message.guild?.prefix || "~"}upgrs\` to view a list of assignable upgrades`, allowedMentions: { parse: [] } });
+		const data = await client.db.getUserData(user.id);
+		let cst = data.get("cst")?.split(";") || [];
+		const tupgr = upgr.find((f) => f[0] === kw);
+		if (cst.includes(tupgr[1])) {
+			cst = cst.filter((f) => ![tupgr[1]].includes(f));
+			message.reply({
+				embeds: [
+					new MessageEmbed()
+						.setColor(message.author.color)
+						.setDescription(`${user.tag} has lost the ${kw} upgrade`),
+				],
+			});
+		}
+		else {
+			cst.push(tupgr[1]);
+			message.reply({
+				embeds: [
+					new MessageEmbed()
+						.setColor(message.author.color)
+						.setDescription(`${user.tag} has received the ${kw} upgrade`),
+				],
+			});
+		}
+		client.db.USERS.update({
+			cst: cst.join(";")
+		}, {
+			where: {
+				id: user.id
+			}
+		})
+	},
+};
--- cmds/upgrs.js
+++ cmds/upgrs.js
@@ -2,19 +2,21 @@
 import { MessageEmbed } from "discord.js";
 
 export default {
-  name: "upgrs",
-  aliases: ["upgrs", "assigns", "ccmds"],
-  description: "View a list of assignable upgrades",
-  category: "utl",
-  async run(client, message, args) {
-    let upgr = await client.db.get("upgr" + message.author.id) || "";
-    if (upgr.split(";").length < 2) return message.reply("You have no assignable upgrades!");
-    let keys = client.config.listToMatrix(upgr.split(";"), 2).map((f) => f[0]);
-    message.reply({
-      embed: new MessageEmbed()
-      .setColor(message.author.color)
-      .setTitle(`${message.author.tag}'s Assignable Upgrades`)
-      .setDescription("```\n" + client.config.Inspect(keys) + "\n```")
-    });
-  }
+	name: "upgrs",
+	aliases: ["upgrs", "assigns", "ccmds"],
+	description: "View a list of assignable upgrades",
+	category: "utl",
+	async run(client, message, args) {
+		const upgr = message.author.data.get("upgr");
+		if (!upgr || upgr?.split(";").length < 2) return message.reply("You have no assignable upgrades!");
+		const keys = client.config.listToMatrix(upgr.split(";"), 2).map((f) => f[0]);
+		message.reply({
+			embeds: [
+				new MessageEmbed()
+					.setColor(message.author.color)
+					.setTitle(`${message.author.tag}'s Assignable Upgrades`)
+					.setDescription("```\n" + client.config.Inspect(keys) + "\n```"),
+			],
+		});
+	},
 };
