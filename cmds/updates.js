ef1179585086c3f341e9c6695d9fdff1c4059ee2
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-07 11:08:52 +0000

    Fix updates

--- cmds/updates.js
+++ cmds/updates.js
@@ -3,33 +3,42 @@ import { MessageEmbed } from "discord.js";
 
 export default {
 	name: "updates",
-	aliases: ['updates', 'announcements'],
-	category: 'utl',
+	aliases: ["updates", "announcements"],
+	category: "utl",
 	description: "Adds/removes the updates role. Haing it means you'll get pinged when there are new updates and additions to the bot.",
-	async run(client, message, args) {
+	async run(client, message) {
 		const guild = client.guilds.cache.get(client.config.statics.supportServer);
-		const mem = guild.member(message.author.id);
-		if (!mem) return message.reply(`Looks like you're not in my support server. Why not join? :D ${client.config.ssInvite}`);
-		let cst = await client.db.get("cst" + message.author.id) || "";
-				cst = cst.split(";");
-		if (cst.includes("updt")) {
+		const mem = await guild.members.fetch(message.author.id).catch(() => {return;});
+		if (!mem) return message.reply(`I cannot find a user by ID "${message.author.id}" in the support server.`);
+		let cst = message.author.data.get("cst")?.split(";") || [];
+		if (mem.roles.cache.has(client.config.statics.defaults.roles.updates)) {
 			cst = cst.filter((x) => x != "updt");
-			await mem.roles.remove(client.config.roles.updates);
+			mem.roles.remove(client.config.statics.defaults.roles.updates);
 			message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} will no longer get mentioned for future updates and announcements.`)
-			})
-			await client.db.set("cst" + message.author.id, cst.join(";"))
-		} else {
-			mem.roles.add(client.config.roles.updates) 
+				embeds: [
+					new MessageEmbed()
+						.setColor(message.author.color)
+						.setDescription(`${message.author.tag} will no longer get mentioned for future updates and announcements`),
+				],
+			});
+		}
+		else {
+			mem.roles.add(client.config.statics.defaults.roles.updates);
 			cst.push("updt");
 			await message.reply({
-				embed: new MessageEmbed()
-				.setColor(message.author.color)
-				.setDescription(`${message.author.tag} will now get mentioned for future updates and announcements.`)
-			})
-			await client.db.set("cst" + message.author.id, cst.join(";"))
-		};
+				embeds: [
+					new MessageEmbed()
+						.setColor(message.author.color)
+						.setDescription(`${message.author.tag} will now get mentioned for future updates and announcements`),
+				],
+			});
+			await client.db.USERS.update({
+				cst: cst.join(";"),
+			}, {
+				where: {
+					id: message.author.id,
+				},
+			});
+		}
 	},
 };
