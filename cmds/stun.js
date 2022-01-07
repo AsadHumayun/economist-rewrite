d93511b46a08e8a3307266230d76b5fa54623cc5
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-07 11:09:37 +0000

    Edit invalid arg error msg

--- cmds/stun.js
+++ cmds/stun.js
@@ -6,7 +6,7 @@ export default {
 	description: "stuns a user, preventing them from using any commands",
 	category: "own",
 	async run(client, message, args) {
-		if (!args.length || (isNaN(args[1]))) return message.reply("You must specify the user to stun, along with the stun time (in minutes)");
+		if (!args.length || (isNaN(args[1]))) return message.reply("You must specify the user to stun, along with the stun time (in minutes) in order for this command to work!");
 		const usr = await client.config.fetchUser(args[0]).catch(() => {return;});
 		if (!usr) return message.reply({ content: `Invalid user "${args[0]}"`, allowedMentions: { parse: [] } });
 		await client.db.USERS.update({
