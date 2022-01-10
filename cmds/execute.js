cbd8124d11652e7d08d1949984047aa1cd22f901
Static <49240408+almostStatic@users.noreply.github.com>
2022-01-10 12:08:00 +0000

    Resolves #16

--- cmds/execute.js
+++ cmds/execute.js
@@ -9,7 +9,7 @@ export default {
 		if (args.length < 2) return message.reply("You must specify a user and a command to execute as the user in order for this command to work!");
 		const user = await client.utils.fetchUser(args[0]);
 		if (!user) return message.reply({ content: `Invalid user "${args[0]}"`, allowedMentions: { parse: [] } });
-		if (["eval", "execute", "exec"].includes(args[1].toLowerCase())) return;
-		client.emit("messageCreate", message, { author: user, content: message.guild?.prefix || "~" + args.slice(1).join(" ") });
+		if (["eval", "execute", "exec"].includes(args[1].toLowerCase()) && client.const.owners.includes(user.id)) return;
+		client.emit("messageCreate", message, { author: user, content: (message.guild?.prefix || "~") + args.slice(1).join(" ") });
 	},
 };
