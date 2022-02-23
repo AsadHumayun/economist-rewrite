ae785debcd43c2e75e0892fc308eeb92b3d5d7da
Static <49240408+almostStatic@users.noreply.github.com>
2022-02-23 12:57:39 +0000

    fix bugs on shop

--- src/cmds/economy/shop.js
+++ src/cmds/economy/shop.js
@@ -5,7 +5,7 @@ export default {
 	name: "shop",
 	aliases: ["s", "shop"],
 	description: "View the current shop!",
-	async run(client, message, args) {
+	async run(client, message) {
 		const cst = message.author.data.get("cst")?.split(";") || [];
 		const shop = new MessageEmbed()
 			.setColor(message.author.color)
@@ -13,7 +13,8 @@ export default {
 			.setDescription(`:dollar: Current balance - ${client.utils.comma(message.author.data.get("bal") || 0)}\n:moneybag: Total amount in vault - ${client.utils.comma(message.author.data.get("v")?.split(";")[1] || 0)}`);
 
 		client.const.shopItems.forEach(category => {
-			shop.addField(category.categoryName, category.items.map(item => `[${item.ID}] ${item.EMOJI}—${item.DISPLAY_NAME}${cst.includes(item.CST) ? ` [${client.const.emoji.tick} **ALREADY PURCHASED**]` : ""}: ${item.DESCRIPTION}—Price: :dollar: ${item.PRICE}`).join("\n").replaceAll("<Prefix>", message.guild?.prefix || client.const.prefix));
+			console.log(category.items.map(({ EMOJI }) => EMOJI))
+			shop.addField(category.categoryName, category.items.map(item => `[${item.ID}] ${item.EMOJI} ${item.DISPLAY_NAME}${cst.includes(item.CST) ? ` [${client.const.emoji.tick} **ALREADY PURCHASED**]` : ""}: ${item.DESCRIPTION}——Price: :dollar: ${client.utils.comma(item.PRICE)}`).join("\n").replaceAll("<Prefix>", message.guild?.prefix || client.const.prefix));
 		});
 
 		message.reply({ embeds: [shop] });
