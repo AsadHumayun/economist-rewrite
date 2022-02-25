82d204c515e232bb0eaf3ea76f7194f6fe4ade84
Static <49240408+almostStatic@users.noreply.github.com>
2022-02-25 14:50:53 +0000

    Update clr & clrs subsequent to making random colours a CST (#48)

--- src/cmds/utilities/clrs.js
+++ src/cmds/utilities/clrs.js
@@ -19,7 +19,7 @@ export default {
 				new MessageEmbed()
 					.setColor(message.author.color)
 					.setTitle(`${usr.tag}'s Colour Preferences`)
-					.setDescription(`Every time you use a command, each colour is cycled through sequentially. The last value is where the bot is currently at in your cycle. \n\n\`\`\`js\n${client.utils.Inspect(data.get("clr").split(";"))}\`\`\``),
+					.setDescription(`Every time you use a command, each colour is cycled through sequentially. The last value is where the bot is currently at in your cycle. \n\n\`\`\`js\n${data.get("cst")?.split(";").includes("rc") ? `You currently have random colours enabled. To disable this, you can sell the random colour preference shop item and gain your money back. Use ${message.guild?.prefix || client.const.prefix}help sell for more information.` : client.utils.Inspect(data.get("clr").split(";"))}\`\`\``),
 			],
 		});
 	},
--- src/cmds/utilities/color.js
+++ src/cmds/utilities/color.js
@@ -15,7 +15,7 @@ export default {
 					new MessageEmbed()
 						.setColor(message.author.color)
 						.setTitle(`${message.author.tag}'s Colour Preferences`)
-						.setDescription(`Every time you use a command, each colour is cycled through sequentially. The last value is where the bot is currently at in your cycle. \n\n\`\`\`js\n${client.utils.Inspect(clrs.split(";"))}\`\`\``),
+						.setDescription(`Every time you use a command, each colour is cycled through sequentially. The last value is where the bot is currently at in your cycle. \n\n\`\`\`js\n${message.author.color === "RANDOM" ? `You currently have random colours enabled. To disable this, you can sell the random colour preference shop item and gain your money back. Use ${message.guild?.prefix || client.const.prefix}help sell for more information.` : client.utils.Inspect(clrs.split(";"))}\`\`\``),
 				],
 			});
 		}
