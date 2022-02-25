e045206c6671ce5bbdc276ea8e8f0e90a375c892
Static <49240408+almostStatic@users.noreply.github.com>
2022-02-25 14:33:19 +0000

    Fix shop (final) (#48)

--- src/cmds/economy/shop.js
+++ src/cmds/economy/shop.js
@@ -1,7 +1,5 @@
 "use strict";
 import { MessageEmbed } from "discord.js";
-import { inspect } from "util";
-import { Util } from "discord.js";
 
 export default {
 	name: "shop",
@@ -15,22 +13,9 @@ export default {
 			.setDescription(`:dollar: Current balance - ${client.utils.comma(message.author.data.get("bal") || 0)}\n:moneybag: Total amount in vault - ${client.utils.comma(message.author.data.get("v")?.split(";")[1] || 0)}`);
 
 		client.const.shopItems.forEach(category => {
-	//		category.items.forEach(console.log)
-			shop.addField(
-				category.categoryName,
-				category.items.map(item => `[${item.ID}] ${item.EMOJI} ${item.DISPLAY_NAME}${cst.includes(item.CST) ? ` [${client.const.emoji.tick} **ALREADY PURCHASED**]` : ""}: ${item.DESCRIPTION}——Price: :dollar: ${client.utils.comma(item.PRICE)}`).join("\n").replaceAll("<Prefix>", message.guild?.prefix || client.const.prefix)
-			)
-		})
-		/*
-		client.const.shopItems.forEach(category => {
-		//	console.log(category.items, Array.isArray(category.items))
-			shop.addField(
-				category.categoryName,
-				category.items.map(item => {
-				console.log(item)
-			return //.join("\n").replaceAll("<Prefix>", message.guild?.prefix || client.const.prefix));
-		}))
-		*/
+			shop.addField(category.categoryName, category.items.map(item => `[${item.ID}] ${item.EMOJI} ${item.DISPLAY_NAME}${cst.includes(item.CST) ? ` [${client.const.emoji.tick} **ALREADY PURCHASED**]` : ""}: ${item.DESCRIPTION}——Price: :dollar: ${client.utils.comma(item.PRICE)}`).join("\n").replaceAll("<Prefix>", message.guild?.prefix || client.const.prefix));
+		});
+
 		message.reply({ embeds: [shop] });
 	},
 };
