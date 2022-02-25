74b411ee4caad1cf3e5262e24476cf7ac97092a6
Static <49240408+almostStatic@users.noreply.github.com>
2022-02-25 17:33:40 +0000

    Update petaliases.js

--- src/Utils/petaliases.js
+++ src/Utils/petaliases.js
@@ -1,21 +1,17 @@
-/**
- * @todo convert this from JSON format to normal JS format, ie just remove all the "" from the var names.
- */
-
 export default {
-	"default": {
-		"$ORIGINAL_PURCHASER_ID": null,
-		"DISPLAY_NAME": "Dragon",
+	default: {
+		$ORIGINAL_PURCHASER_ID: null,
+		DISPLAY_NAME: "Dragon",
 		/**
 	 * Array consisting of all the default emojis for the dragon system.
 	 * @const {string[]}
 	 */
-		"EMOJIS": ["<:heart:912982056802340877>", ":zap:", ":star2:", ":star:", ":bulb:", ":field_hockey:", ":fire:", ":sparkling_heart:", ":pizza:"],
+		EMOJIS: ["<:heart:912982056802340877>", ":zap:", ":star2:", ":star:", ":bulb:", ":field_hockey:", ":fire:", ":sparkling_heart:", ":pizza:"],
 	},
-	"dwerd": {
-		"$ORIGINAL_PURCHASER_ID": "50461983300701389910994293129216;671708767813107724",
-		"DISPLAY_NAME": "Dwerd",
-		"EMOJIS": [
+	dwerd: {
+		$ORIGINAL_PURCHASER_ID: "50461983300701389910994293129216;671708767813107724",
+		DISPLAY_NAME: "Dwerd",
+		EMOJIS: [
 			":watermelon:",
 			":guitar:",
 			"<a:hero:780398361219825674>",
@@ -27,13 +23,13 @@ export default {
 			":pizza:",
 		],
 	},
-	"t1": {
+	t1: {
 
 	},
-	"t2": {
+	t2: {
 
 	},
-	"t3": {
+	t3: {
 
 	},
 };
