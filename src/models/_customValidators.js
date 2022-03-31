827228359b75304e74d2bbb3f18c72c64d991708
Static <49240408+almostStatic@users.noreply.github.com>
2022-03-31 20:14:54 +0100

    Comment out debug statements (_customValidators._isNumber) (#55)

--- src/models/_customValidators.js
+++ src/models/_customValidators.js
@@ -10,14 +10,20 @@ export function isNumericString(data) {
 }
 
 export function _isNumber(data) {
+	// console.log("customValidator0", data, typeof data)
 	let invalid = false;
 
 	if (isNaN(Number(data))) {
 		invalid = true;
+		// console.log("customValidator1", data, typeof data)
 		return;
 	}
 	else if (data.toString().split("&").length !== 2 && isNaN(data)) {
 		invalid = true;
+		// console.log("customValidator2", data, typeof data)
 	}
+	// console.log("customValidator3", data, typeof data)
 	if (invalid === true) throw new Error(`\n[CustomValidator => isNumber] [Fail] Value must be a valid JavaScript number or formatted via utils/Functions/format. Received type: ${typeof data}`);
+	// console.log("customValidator4", data, typeof data)
+	return data;
 }
