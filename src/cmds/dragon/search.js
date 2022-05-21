a2eef9304746a79d708d87c3a3905c95ab60998c
Asad <105254706+asadhumayun@users.noreply.github.com>
2022-05-21 22:29:39 +0100

    Move search onto new Number system (#59)

--- src/cmds/dragon/search.js
+++ src/cmds/dragon/search.js
@@ -1,4 +1,5 @@
 "use strict";
+
 import { MessageEmbed } from "discord.js";
 import delay from "delay";
 
@@ -10,35 +11,34 @@ export default {
 	async run(client, message) {
 		const alias = await client.utils.getDragonAlias(message.author.id);
 		const cd = message.author.data.get("srchc") || 0;
-		const scnd = client.utils.cooldown(message.createdTimestamp, cd * 60_000n);
+		const scnd = client.utils.cooldown(message.createdTimestamp, cd * 60_000);
 		if (scnd) {
 			return message.reply(`Please wait another ${scnd} before searching again!`);
 		}
 		let data = message.author.data.get("drgn");
 		if (message.author.data.get("cst")?.includes("maxdragon888")) data = client.const.naxDragon;
 		if (!data) data = client.const.dragon;
-		data = data.split(";").map(BigInt);
+		data = data.split(";").map(client.utils.expand);
 		const en = data[2];
 		const endur = data[6];
 		const lvl = data[0];
 		let xp = data[3];
 		const str = data[7];
 		const intel = data[5];
-		const consumed = Math.round((60n / BigInt(Math.log(endur + 9))));
+		const consumed = BigInt(Math.round(String(60n / BigInt(Math.round(Math.log(Number(endur) + 9))))));
 		if (en - consumed < 0n) {
 			return message.reply("🥱 I'm too tired to go searching right now! Why not feed me by using `" + message.guild ? message.guild.prefix : client.const.prefix + "feed`?");
 		}
-		// parseFloat(((message.createdTimestamp + ms("20s"))/60_000)).toFixed(2)
-		const f = (message.author.data.get("fsh") || "0;0;0;0;0;0").split(";").map(BigInt);
-		const fishes = [":dolphin:", ":shark:", ":blowfish:", ":tropical_fish:", ":fish:"];
-		const Fish = Math.floor(Math.random() * fishes.length);
-		const fish = fishes[Fish];
-		const amtGained = Math.floor(Math.random() * 250 / 5) * str;
-		f[Fish] += amtGained;
-		const xpGained = BigInt(Math.floor(BigInt(intel) * 2n * 50n * BigInt(0.5)) * lvl);
+		const fishes = client.const.shopItems[1].items;
+		const drgs = message.author.data.get("drgs")?.split(";").map(client.utils.expand) || new Array(20).fill(0n);
+		const __fishIndex = Math.floor(Math.random() * fishes.length);
+		const fish = fishes[__fishIndex];
+		const amtGained = BigInt(Math.floor(Math.random() * 250 / 5)) * str;
+		drgs[__fishIndex] += BigInt(amtGained);
+		const xpGained = intel * 2n * 50n * lvl;
 		data[2] = en - consumed;
 		await client.db.USERS.update({
-			fsh: f.join(";"),
+			drgs: client.utils.removeZeros(drgs).join(";"),
 			// set cooldown for 20s.
 			srchc: client.utils.parseCd(message.createdTimestamp, 20_000, true),
 		}, {
@@ -66,7 +66,7 @@ export default {
 			embeds: [
 				new MessageEmbed()
 					.setColor(message.author.color)
-					.setDescription(`${message.author.tag}'s ${alias[0]} has found out that ${fish} ${client.utils.noExponents(amtGained)} are dwelling in the lake`),
+					.setDescription(`${message.author.tag}'s ${alias[0]} has found out that ${fish.EMOJI} ${client.utils.noExponents(amtGained)} are dwelling in the lake`),
 			],
 		});
 		await delay(1500);
@@ -74,7 +74,7 @@ export default {
 			embeds: [
 				new MessageEmbed()
 					.setColor(message.author.color)
-					.setDescription(`${message.author.tag}'s ${alias[0]} instantaneously lets out a mighty roar, <a:ecn_fire:804378228336361476> searing ${fish} ${client.utils.noExponents(amtGained)} and obtained ${alias[1][2]} ${client.utils.noExponents(xpGained) || "0"} in the process`),
+					.setDescription(`${message.author.tag}'s ${alias[0]} instantaneously lets out a mighty roar, <a:ecn_fire:804378228336361476> searing ${fish.EMOJI} ${client.utils.noExponents(amtGained)} and obtained ${alias[1][2]} ${client.utils.noExponents(xpGained) || "0"} in the process`),
 			],
 		});
 		data[3] = xp + xpGained;
@@ -104,7 +104,7 @@ export default {
 				});
 			}
 			await client.db.USERS.update({
-				drgn: data.map(String).join(";"),
+				drgn: data.map(client.utils.format).join(";"),
 			}, {
 				where: {
 					id: message.author.id,
