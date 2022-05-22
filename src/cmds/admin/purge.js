80107ab0a8462ac87172251b2a21185ae47d1da7
Asad <105254706+AsadHumayun@users.noreply.github.com>
2022-05-22 23:21:06 +0100

    "Updated" -> "Edited"

--- src/cmds/admin/purge.js
+++ src/cmds/admin/purge.js
@@ -43,7 +43,7 @@ export default {
 				embeds: [
 					new MessageEmbed()
 						.setColor(message.author.color)
-						.setDescription(`Updated ${data.count} accounts`),
+						.setDescription(`Edited ${updated} accounts`),
 				],
 			});
 		});
